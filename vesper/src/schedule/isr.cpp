27473ae32850f2513838f3a38c6c4b66ac6ed73e
Berkus <berkus@madfire.net>
2008-08-29 01:41:40 +0300

    Serve ISRs before acknowledging to PIC.

--- vesper/src/schedule/isr.cpp
+++ vesper/src/schedule/isr.cpp
@@ -18,9 +18,9 @@ void isr_handler(registers_t regs)
 	kconsole.set_color(GREEN);
 	kconsole.print("Received interrupt: %d\n", regs.int_no);
 
-	if (interrupt_handlers[regs.int_no] != 0)
+	isr_t handler = interrupt_handlers[regs.int_no];
+	if (handler)
 	{
-		isr_t handler = interrupt_handlers[regs.int_no];
 		handler(regs);
 	}
 }
@@ -28,6 +28,12 @@ void isr_handler(registers_t regs)
 // This gets called from our ASM hardware interrupt handler stub.
 void irq_handler(registers_t regs)
 {
+	isr_t handler = interrupt_handlers[regs.int_no];
+	if (handler)
+	{
+		handler(regs);
+	}
+
 	// Send an EOI (end of interrupt) signal to the PICs.
 	// If this interrupt involved the slave.
 	if (regs.int_no >= IRQ8)
@@ -37,10 +43,4 @@ void irq_handler(registers_t regs)
 	}
 	// Send reset signal to master. (As well as slave, if necessary).
 	outb(0x20, 0x20);
-
-	if (interrupt_handlers[regs.int_no] != 0)
-	{
-		isr_t handler = interrupt_handlers[regs.int_no];
-		handler(regs);
-	}
 }
