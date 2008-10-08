cd196090137a1ba8d041210e7ada1f23c6756579
Berkus <berkus@madfire.net>
2008-10-08 14:12:42 +0300

    Use x86-release configuration variant for building.

--- vesper/src/update_image.sh
+++ vesper/src/update_image.sh
@@ -3,7 +3,7 @@
 echo "Updating bootable image, enter your sudo password to continue:"
 mkdir -p mountpoint
 sudo mount -o loop floppy.img mountpoint
-sudo cp -f build/default/vesper mountpoint/kernel
+sudo cp -f build/x86-release/vesper mountpoint/kernel
 sudo umount mountpoint
 rmdir mountpoint
 touch floppy.img
