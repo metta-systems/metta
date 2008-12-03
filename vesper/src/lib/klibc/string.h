25963696fc76e67609558905b5bb2e06ba979aeb
Berkus <berkus@madfire.net>
2008-12-03 16:19:50 +0200

    Add memory operations.

--- vesper/src/lib/klibc/string.h
+++ vesper/src/lib/klibc/string.h
@@ -6,25 +6,92 @@
 //
 #pragma once
 
-#include "Macros.h"
+#include "Types.h"
 
-/** Encapsulate string manipulation methods. */
-class string
-{
-public:
-    string() : len(0), str(0) {}
+/*
+ * TODO Include machine specific inline routines
+ */
+// #include <asm/string.h>
 
-    INLINE int length() { return len; }
-    int cmp(const string& other);
-    bool equals(const string& other);
+#ifndef __HAVE_ARCH_STRCPY
+extern char * strcpy(char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCPY
+extern char * strncpy(char *,const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCAT
+extern char * strcat(char *, const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCAT
+extern char * strncat(char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRLCAT
+extern size_t strlcat(char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCMP
+extern int strcmp(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNCMP
+extern int strncmp(const char *,const char *,size_t);
+#endif
+#ifndef __HAVE_ARCH_STRNICMP
+extern int strnicmp(const char *, const char *, size_t);
+#endif
+#ifndef __HAVE_ARCH_STRCASECMP
+extern int strcasecmp(const char *s1, const char *s2);
+#endif
+#ifndef __HAVE_ARCH_STRNCASECMP
+extern int strncasecmp(const char *s1, const char *s2, size_t n);
+#endif
+#ifndef __HAVE_ARCH_STRCHR
+extern char * strchr(const char *,int);
+#endif
+#ifndef __HAVE_ARCH_STRNCHR
+extern char * strnchr(const char *, size_t, int);
+#endif
+#ifndef __HAVE_ARCH_STRRCHR
+extern char * strrchr(const char *,int);
+#endif
+extern char * strstrip(char *);
+#ifndef __HAVE_ARCH_STRSTR
+extern char * strstr(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRLEN
+extern size_t strlen(const char *);
+#endif
+#ifndef __HAVE_ARCH_STRNLEN
+extern size_t strnlen(const char *,size_t);
+#endif
+#ifndef __HAVE_ARCH_STRPBRK
+extern char * strpbrk(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRSEP
+extern char * strsep(char **,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRSPN
+extern size_t strspn(const char *,const char *);
+#endif
+#ifndef __HAVE_ARCH_STRCSPN
+extern size_t strcspn(const char *,const char *);
+#endif
 
-    static int length(const char *str);
-    static bool equals(const char *in1, const char *in2);
+#ifndef __HAVE_ARCH_MEMSCAN
+extern void * memscan(void *,int,size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMCMP
+extern int memcmp(const void *,const void *,size_t);
+#endif
+#ifndef __HAVE_ARCH_MEMCHR
+extern void * memchr(const void *,int,size_t);
+#endif
 
-private:
-    int len;
-    char *str;
-};
+// defined in memory.c
+extern void* memset(void* dest, int value, size_t count);
+extern void* memcpy(void* dest, const void* src, size_t count);
+extern void* memmove(void* dest, const void* src, size_t count);
 
 // kate: indent-width 4; replace-tabs on;
 // vi:set ts=4:set expandtab=on:
