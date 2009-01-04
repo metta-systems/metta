120ccbc0a4d300ed34430a8269d5f845d6e9c87a
Berkus <berkus@madfire.net>
2009-01-05 01:33:49 +0200

    Reformat source.

--- vesper/src/lib/Lockable.h
+++ vesper/src/lib/Lockable.h
@@ -9,56 +9,54 @@
 #include "atomic.h"
 
 /**
- * A class that implements a spinlock / binary semaphore.
- */
+* A class that implements a spinlock / binary semaphore.
+**/
 class lockable
 {
 public:
-	lockable()
-	{
-		lock = 0;
-	}
+    lockable()
+    {
+        lock = 0;
+    }
 
-	~lockable() {}
+    // Spin until we get the lock.
+    void get_lock()
+    {
+        uint32_t new_val = 1;
+        // If we exchange the lock value with 1 and get 1 out, it was locked.
+        while (atomic_ops::exchange(&lock, new_val) == 1)
+        {
+            // Do nothing.
+        }
+        // We got the lock, return.
+    }
 
-	// Spin until we get the lock.
-	void get_lock()
-	{
-		uint32_t new_val = 1;
-		// If we exchange the lock value with 1 and get 1 out, it was locked.
-		while (atomic_ops::exchange(&lock, new_val) == 1)
-		{
-			// Do nothing.
-		}
-		// We got the lock, return.
-	}
+    bool try_lock()
+    {
+        // Spin once.
+        uint32_t new_val = 1;
+        if (atomic_ops::exchange(&lock, new_val) == 0)
+        {
+            return true;
+        }
+        return false;
+    }
 
-	bool try_lock()
-	{
-		// Spin once.
-		uint32_t new_val = 1;
-		if (atomic_ops::exchange(&lock, new_val) == 0)
-		{
-			return true;
-		}
-		return false;
-	}
+    bool test_lock()
+    {
+        return lock;
+    }
 
-	bool test_lock()
-	{
-		return lock;
-	}
-
-	void release_lock()
-	{
-		lock = 0;
-	}
+    void release_lock()
+    {
+        lock = 0;
+    }
 
 private:
-	/**
-	 * The actual lock variable
-	 */
-	uint32_t lock;
+    /**
+    * The actual lock variable
+    */
+    uint32_t lock;
 };
 
 // kate: indent-width 4; replace-tabs on;
