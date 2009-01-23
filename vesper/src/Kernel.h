5bb0572095afe61107378a389ea2c234faafee58
Berkus <berkus@madfire.net>
2009-01-23 19:13:18 +0200

    Get rid of INLINE macro. Cleanup doxygen doc. Remove tabs.

--- vesper/src/Kernel.h
+++ vesper/src/Kernel.h
@@ -16,81 +16,81 @@ namespace kernel {
 class kernel
 {
 public:
-	/**
-	 * Boot the kernel
-	 */
-	void run() NORETURN;
+    /**
+    * Boot the kernel.
+    **/
+    void run() NORETURN;
 
-	/**
-	 * Dump @c size bytes from a memory region starting at virtual address @c start.
-	 */
-	static void dump_memory(address_t start, size_t size);
+    /**
+    * Dump @p size bytes from a memory region starting at virtual address @p start.
+    **/
+    static void dump_memory(address_t start, size_t size);
 
-	/**
-	 * Given a stack base pointer, follow it, return the next stack base
-	 * pointer and also return the instruction pointer it returned to.
-	 */
-	static address_t backtrace(address_t base_pointer, address_t& return_address);
+    /**
+    * Given a stack base pointer, follow it, return the next stack base
+    * pointer and also return the instruction pointer it returned to.
+    **/
+    static address_t backtrace(address_t base_pointer, address_t& return_address);
 
-	/**
-	 * Given the current stack, follow 'n' backtraces and return the
-	 * return address found there.
-	 */
-	static address_t backtrace(int n);
+    /**
+    * Given the current stack, follow 'n' backtraces and return the
+    * return address found there.
+    **/
+    static address_t backtrace(int n);
 
-	/**
-	 * memset - Fill a region of memory with the given value
-	 * @s: Pointer to the start of the area.
-	 * @c: The byte to fill the area with
-	 * @count: The size of the area.
-	 *
-	 * Do not use memset() to access IO space, use memset_io() instead.
-	 */
-	INLINE static void* set_memory(void* dest, int value, size_t count)
-	{
+    /**
+    * memset - Fill a region of memory with the given value
+    * @p dest Pointer to the start of the area.
+    * @p value The byte to fill the area with
+    * @p count The size of the area.
+    *
+    * Do not use memset() to access IO space, use memset_io() instead.
+    **/
+    inline static void* set_memory(void* dest, int value, size_t count)
+    {
         return memset(dest, value, count);
-	}
+    }
 
-	/**
-	 * memcpy - Copy one area of memory to another
-	 * @dest: Where to copy to
-	 * @src: Where to copy from
-	 * @count: The size of the area.
-	 *
-	 * You should not use this function to access IO space, use memcpy_toio()
-	 * or memcpy_fromio() instead.
-	 */
-	INLINE static void* copy_memory(void* dest, const void* src, size_t count)
-	{
+    /**
+    * memcpy - Copy one area of memory to another
+    * @p dest Where to copy to
+    * @p src Where to copy from
+    * @p count The size of the area.
+    *
+    * You should not use this function to access IO space, use memcpy_toio()
+    * or memcpy_fromio() instead.
+    **/
+    inline static void* copy_memory(void* dest, const void* src, size_t count)
+    {
         return memcpy(dest, src, count);
-	}
+    }
 
-	/**
-	 * memmove - Copy one area of memory to another
-	 * @dest: Where to copy to
-	 * @src: Where to copy from
-	 * @count: The size of the area.
-	 *
-	 * Unlike memcpy(), memmove() copes with overlapping areas.
-	 */
-	INLINE static void* move_memory(void* dest, const void* src, size_t count)
-	{
+    /**
+    * memmove - Copy one area of memory to another
+    * @p dest Where to copy to
+    * @p src Where to copy from
+    * @p count The size of the area.
+    *
+    * Unlike memcpy(), memmove() copes with overlapping areas.
+    **/
+    inline static void* move_memory(void* dest, const void* src, size_t count)
+    {
         return memmove(dest, src, count);
-	}
+    }
 
-	/**
-	 * Print a full backtrace from the current location. (Or, if @p n is specified,
-	 * up to n stack frames.
-	 */
-	void print_backtrace(address_t base_pointer = 0, int n = 0);
+    /**
+    * Print a full backtrace from the current location. (Or, if @p n is specified,
+    * up to n stack frames.
+    **/
+    void print_backtrace(address_t base_pointer = 0, int n = 0);
 
-	/**
-	 * Prints first n words from the stack
-	 */
-	void print_stacktrace(unsigned int n = 64);
+    /**
+    * Prints first @p n words from the stack
+    **/
+    void print_stacktrace(unsigned int n = 64);
 
 private:
-	void relocate_placement_address();
+    void relocate_placement_address();
 };
 
 }
