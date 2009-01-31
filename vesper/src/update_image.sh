1f48da37143034ff33ea019fdcb3b449f3b31ae5
Berkus <berkus@madfire.net>
2009-01-31 11:19:45 +0200

    Use _build_ directory.

--- vesper/src/update_image.sh
+++ vesper/src/update_image.sh
@@ -3,7 +3,7 @@
 echo "Updating bootable image, enter your sudo password to continue:"
 mkdir -p mountpoint
 sudo mount -o loop floppy.img mountpoint
-sudo cp -f build/x86-release/vesper mountpoint/kernel
+sudo cp -f _build_/x86-release/vesper mountpoint/kernel
 sudo umount mountpoint
 rmdir mountpoint
 touch floppy.img
