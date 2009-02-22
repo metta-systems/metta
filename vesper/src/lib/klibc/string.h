ab9a66523b3239594ddfc261abe6a9a8356a4027
Berkus <berkus@madfire.net>
2009-02-22 14:51:22 +0200

    Do not use bstrlib's string.

--- vesper/src/lib/klibc/string.h
+++ vesper/src/lib/klibc/string.h
@@ -6,25 +6,5 @@
 //
 #pragma once
 
-#ifdef __cplusplus
-#include "types.h"
-#include "bstrwrap.h"
-namespace metta {
-namespace common {
-typedef Bstrlib::CBString string;
-}
-}
-
-extern "C" {
-#endif
-
-// for bstring
-int vsnprintf(char *buf, size_t size, const char *fmt, va_list args);
-int sscanf(const char * buf, const char * fmt, ...);
-
-#ifdef __cplusplus
-}
-#endif
-
 // kate: indent-width 4; replace-tabs on;
 // vim: set et sw=4 ts=4 sts=4 cino=(4 :
