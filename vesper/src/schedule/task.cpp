805b6fac5d7eb86ba0f5be182517d877cab9ba4e
Berkus <berkus@madfire.net>
2009-02-22 06:03:21 +0200

    Clean up unused task code.

--- vesper/src/schedule/task.cpp
+++ vesper/src/schedule/task.cpp
@@ -5,6 +5,7 @@
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include "task.h"
+#include "types.h"
 #include "registers.h"
 #include "memory_manager.h"
 #include "default_console.h"
@@ -12,89 +13,42 @@
 namespace metta {
 namespace kernel {
 
-// The currently running task.
-static volatile task *current_task = 0;
-
-// The start of the task linked list.
-// static volatile task *ready_queue;
-
 // The next available process ID.
 static uint32_t next_pid = 1;
+static task* kernel_task = NULL;
 
 void task::init()
 {
-	// Rather important stuff happening, no interrupts please!
-	critical_section();
+    // Rather important stuff happening, no interrupts please!
+    critical_section();
 
-	// Initialise the first task (kernel task)
-	current_task = /*ready_queue =*/ new task;
-	current_task->id = next_pid++;
-// 	current_task->esp = current_task->ebp = 0;
-// 	current_task->eip = 0;
-	current_task->page_dir = kmemmgr.get_current_directory();
-	current_task->next = 0;
+    // Initialise the first task (kernel task)
+    kernel_task = /*ready_queue =*/ new task;
+    kernel_task->id = next_pid++;
+    assert(kernel_task->id == 1);
+    kernel_task->page_dir = kmemmgr.get_current_directory();
+    kernel_task->next = 0;
 
-	kconsole.debug_log("Constructed kernel task.");
+    kconsole.debug_log("Constructed kernel task.");
 
-	// Reenable interrupts.
-	end_critical_section();
+    end_critical_section();
 }
 
-// void task::yield()
-// {
-//     // If we haven't initialised tasking yet, just return.
-//     if (!current_task)
-//         return;
-//
-//     // Read esp, ebp now for saving later on.
-//     uint32_t esp, ebp, eip;
-//     asm volatile("mov %%esp, %0" : "=r"(esp));
-//     asm volatile("mov %%ebp, %0" : "=r"(ebp));
-//
-//     // Read the instruction pointer. We do some cunning logic here:
-//     // One of two things could have happened when this function exits -
-//     //   (a) We called the function and it returned the EIP as requested.
-//     //   (b) We have just switched tasks, and because the saved EIP is essentially
-//     //       the instruction after read_eip(), it will seem as if read_eip has just
-//     //       returned.
-//     // In the second case we need to return immediately. To detect it we put a dummy
-//     // value in EAX further down at the end of this function. As C returns values in EAX,
-//     // it will look like the return value is this dummy value! (0x12345).
-//     eip = read_instruction_pointer();
-//
-//     // Have we just switched tasks?
-//     if (eip == 0x12345)
-//         return;
-//
-//     // No, we didn't switch tasks. Let's save some register values and switch.
-//     current_task->eip = eip;
-//     current_task->esp = esp;
-//     current_task->ebp = ebp;
-//
-//     // Get the next task to run.
-//     current_task = current_task->next;
-//     // If we fell off the end of the linked list start again at the beginning.
-//     if (!current_task) current_task = ready_queue;
-//
-//     eip = current_task->eip;
-//     esp = current_task->esp;
-//     ebp = current_task->ebp;
-//
-// //  kconsole.print("yield() to %d\n", current_task->id);
-//
-//     // Make sure the memory manager knows we've changed page directory.
-//     kmemmgr.set_current_directory(current_task->page_dir);
-//     // Here we:
-//     // * Stop interrupts so we don't get interrupted.
-//     // * Temporarily put the new EIP location in ECX.
-//     // * Load the stack and base pointers from the new task struct.
-//     // * Change page directory to the physical address (physicalAddr) of the new directory.
-//     // * Put a dummy value (0x12345) in EAX so that above we can recognise that we've just
-//     //   switched task.
-//     // * Restart interrupts. The STI instruction has a delay - it doesn't take effect until after
-//     //   the next instruction.
-//     // * Jump to the location in ECX (remember we put the new EIP in there).
-/*     asm volatile("         \
+/* void task::yield()
+{
+    // Make sure the memory manager knows we've changed page directory.
+    kmemmgr.set_current_directory(current_task->page_dir);
+    // Here we:
+    // * Stop interrupts so we don't get interrupted.
+    // * Temporarily put the new EIP location in ECX.
+    // * Load the stack and base pointers from the new task struct.
+    // * Change page directory to the physical address (physicalAddr) of the new directory.
+    // * Put a dummy value (0x12345) in EAX so that above we can recognise that we've just
+    //   switched task.
+    // * Restart interrupts. The STI instruction has a delay - it doesn't take effect until after
+    //   the next instruction.
+    // * Jump to the location in ECX (remember we put the new EIP in there).
+     asm volatile("         \
       cli;                 \
       mov %0, %%ecx;       \
       mov %1, %%esp;       \
@@ -106,67 +60,24 @@ void task::init()
         : : "r"(eip), "r"(esp), "r"(ebp), "r"(current_task->page_dir->get_physical()));
 }*/
 
-task *task::self()
-{
-	return (task *)current_task;
-}
-
-int task::fork()
+task* task::clone()
 {
     // We are modifying kernel structures, and so cannot be interrupted.
-//     critical_section();
-
-    // Take a pointer to this process' task struct for later reference.
-//     task *parent_task = (task *)current_task;
+    critical_section();
 
     // Clone the address space.
-//     page_directory *directory = kmemmgr.get_current_directory()->clone();
+    page_directory *directory = kmemmgr.get_current_directory()->clone();
 
     // Create a new process.
-//     task *new_task = new task;
+    task *new_task = new task;
 
-//     new_task->id = next_pid++;
-//     new_task->esp = new_task->ebp = 0;
-//     new_task->eip = 0;
-//     new_task->page_dir = directory;
-//     new_task->next = 0;
+    new_task->id = next_pid++;
+    new_task->page_dir = directory;
+    new_task->next = 0;
 
-//     kconsole.print("fork() to %d\n", new_task->id);
+    end_critical_section();
 
-    // Add it to the end of the ready queue.
-//     task *tmp_task = (task *)ready_queue;
-//     while (tmp_task->next)
-//         tmp_task = tmp_task->next;
-//     tmp_task->next = new_task;
-
-    // This will be the entry point for the new process.
-//     uint32_t eip = read_instruction_pointer();
-
-    // We could be the parent or the child here - check.
-/*    if (current_task == parent_task)
-    {
-        // We are the parent, so set up the esp/ebp/eip for our child.
-        uint32_t esp; asm volatile("mov %%esp, %0" : "=r"(esp));
-        uint32_t ebp; asm volatile("mov %%ebp, %0" : "=r"(ebp));
-        new_task->esp = esp;
-        new_task->ebp = ebp;
-        new_task->eip = eip;
-
-        kconsole.print("Saving new task's EIP %08x\n", eip);
-        end_critical_section();
-
-        return new_task->id;
-    }
-    else*/
-    {
-        // We are the child.
-        return 0;
-    }
-}
-
-int task::getpid()
-{
-    return id;
+    return new_task;
 }
 
 }
--- vesper/src/schedule/task.h
+++ vesper/src/schedule/task.h
@@ -22,26 +22,22 @@ class page_directory;
 // This structure defines 'task' - a protection domain.
 class task : public object
 {
-	public:
-		static task *self();
+public:
+    static void init();// Initialises the kernel task.
 
-		static void init();// Initialises the kernel task.
-//		static void yield();// Called by the timer hook, this changes the running process.
+    // Forks the current process, spawning a new one with a different
+    // memory space. New process has no associated threads.
+    task* clone();
+    // RVM concept means that every newly created PD will receive a Parent interface to its parent nester. A fork is therefore merely setting nesting relationship and makes current task control the child task via Parent interface interposition.
 
-		// Forks the current process, spawning a new one with a different
-		// memory space.
-		int fork();
-        // RVM concept means that every newly created PD will receive a Parent interface to its parent nester. A fork is therefore merely setting nesting relationship and makes current task control the child task via Parent interface interposition.
+    // Returns the pid of the current process.
+    inline int getpid() { return id; }
 
-		// Returns the pid of the current process.
-		int getpid();
-
-	private:
-		int id;                        // Process ID.
-//		uint32_t esp, ebp;             // Stack and base pointers.
-//		uint32_t eip;                  // Instruction pointer.
-		page_directory* page_dir;      // Page directory.
-		task *next;                    // The next task in a linked list.
+private:
+    int id;                        ///< Process ID.
+    page_directory* page_dir;      ///< Page directory.
+    task* next;                    ///< The next task in a linked list.
+    priority_queue<thread> runnable_threads; ///< List of runnable threads in task's PD.
 };
 
 }
