e75c50e598eb133cc3df2d27f7a18708465deb3c
Berkus <berkus@madfire.net>
2009-03-31 00:42:03 +0300

    Add boilerplate.

--- src/vesper/memory/page_allocator.h
+++ src/vesper/memory/page_allocator.h
@@ -1,3 +1,9 @@
+//
+// Copyright 2007 - 2009, Stanislav Karchebnyy <berkus+metta@madfire.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #pragma once
 
 #include "types.h"
@@ -59,3 +65,6 @@ private:
     page_allocator_impl *impl;
 };
 
+
+// kate: indent-width 4; replace-tabs on;
+// vim: set et sw=4 ts=4 sts=4 cino=(4 :
