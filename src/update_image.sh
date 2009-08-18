9d66febb2968b3e74304c8965a6879c9fc51a0c9
Berkus <berkus@madfire.net>
2009-08-18 05:24:26 +0300

    Update buildsystem.

--- src/update_image.sh
+++ src/update_image.sh
@@ -3,8 +3,10 @@
 echo "Updating bootable image, enter your sudo password to continue:"
 mkdir -p mountpoint
 sudo mount -o loop floppy.img mountpoint
-sudo cp -f _build_/x86-release/bootloader mountpoint/bootloader
-sudo cp -f _build_/x86-release/initcomp mountpoint/initcomp
+sudo cp -f _build_/x86-pc99-release/vesper/bootloader mountpoint/bootloader
+#sudo cp -f _build_/x86-pc99-release/vesper/nucleus mountpoint/kernel
+sudo cp -f _build_/x86-pc99-release/vesper/initcomp   mountpoint/initcomp
+sudo cp -f _build_/x86-pc99-release/vesper/initfs.img mountpoint/initfs
 sudo umount mountpoint
 rmdir mountpoint
 touch floppy.img
