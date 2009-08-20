6959c3d311a4f926e47af616268ea80619176334
Berkus <berkus@madfire.net>
2009-08-20 14:59:20 +0300

    Remove unused inlines.

--- src/vesper/x86/lib/asm_inlines.h
+++ src/vesper/x86/lib/asm_inlines.h
@@ -10,7 +10,7 @@
 
 /**
 * Write a byte out to the specified port.
-* a puts value in eax, dN puts port in edx or uses 1-byte constant.
+* ("a" puts value in eax, "dN" puts port in edx or uses 1-byte constant.)
 **/
 static inline void outb(uint16_t port, uint8_t value)
 {
@@ -25,16 +25,16 @@ static inline void outw(uint16_t port, uint16_t value)
 
 static inline uint8_t inb(uint16_t port)
 {
-	uint8_t ret;
-	asm volatile("inb %1, %0" : "=a" (ret) : "dN" (port));
-	return ret;
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a" (ret) : "dN" (port));
+    return ret;
 }
 
 static inline uint16_t inw(uint16_t port)
 {
-	uint16_t ret;
-	asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
-	return ret;
+    uint16_t ret;
+    asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
+    return ret;
 }
 
 static inline uint64_t rdtsc()
@@ -44,7 +44,7 @@ static inline uint64_t rdtsc()
     return ret;
 }
 
-static inline void enable_interrupts(void)
+static inline void enable_interrupts(void) //FIXME: ia32_cpu_t::enable_interrupts()
 {
     asm volatile ("sti");
 }
@@ -53,27 +53,6 @@ static inline void disable_interrupts(void)
 {
     asm volatile ("cli");
 }
-
-static inline address_t read_page_directory() // FIXME: use ia32_mmu_t
-{
-    uint32_t ret;
-    asm volatile ("movl %%cr3, %0" : "=r"(ret));
-    return ret;
-}
-
-static inline void flush_page_directory(void) // FIXME: use ia32_mmu_t
-{
-    asm volatile ("movl %%cr3, %%eax\n\t"
-                  "movl %%eax, %%cr3\n");
-}
-
-static inline void enable_paging(void) // FIXME: use ia32_mmu_t
-{
-    asm volatile ("movl %%cr0, %%eax\n\t"
-    "orl $0x80000000, %%eax\n\t"
-    "movl %%eax, %%cr0");
-}
-
 
 // kate: indent-width 4; replace-tabs on;
 // vim: set et sw=4 ts=4 sts=4 cino=(4 :
