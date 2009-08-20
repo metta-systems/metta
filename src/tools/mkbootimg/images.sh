094c4b7ba33806bd6dcf6ee4112dd073ee79f5d2
Berkus <berkus@madfire.net>
2009-08-20 17:35:22 +0300

    Reorg build process a little.

--- src/tools/mkbootimg/images.sh
+++ src/tools/mkbootimg/images.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
 # Script taken from Pedigree and slightly modified
 
-IMAGEDIR=../../images
 BUILDDIR=../../_build_/x86-pc99-release
+FLOPPY=$BUILDDIR/floppy.img
+CDISO=$BUILDDIR/metta.iso
 
 # Create directory fd
-mkdir fd
+mkdir -p fd
 chmod 0777 fd
 
 # Create empty image
-dd if=/dev/zero of=$IMAGEDIR/floppy.img bs=1024 count=1440
+dd if=/dev/zero of=$FLOPPY bs=1024 count=1440
 
 # Format the image
-mke2fs -F $IMAGEDIR/floppy.img
+mke2fs -F $FLOPPY
 
 # Mount the Ext2 image
-sudo mount -oloop $IMAGEDIR/floppy.img fd
+sudo mount -oloop $FLOPPY fd
 mkdir -p fd/boot/grub
 cp menu.lst stage{1,2} fd/boot/grub
 cp $BUILDDIR/vesper/x86/kickstart.bin fd/kickstart
 cp $BUILDDIR/vesper/x86/orb.bin       fd/orb
 cp $BUILDDIR/initfs.img               fd/bootcomps
 sudo umount fd
+rm -rf fd
 
 # Install grub
 grub --batch --no-floppy <<EOT 1>/dev/null  || exit 1
-device (fd0) $IMAGEDIR/floppy.img
+device (fd0) $FLOPPY
 install (fd0)/boot/grub/stage1 (fd0) (fd0)/boot/grub/stage2 (fd0)/boot/grub/menu.lst
 quit
 EOT
-
-# Delete directory fd
-rmdir fd
 
 # Create an ISO image
 mkdir -p iso/boot/grub
@@ -40,5 +39,5 @@ cp menu.lst.cd iso/boot/grub/menu.lst
 cp $BUILDDIR/vesper/x86/kickstart.bin iso/kickstart
 cp $BUILDDIR/vesper/x86/orb.bin       iso/orb
 cp $BUILDDIR/initfs.img               iso/bootcomps
-mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o $IMAGEDIR/metta.iso iso
+mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o $CDISO iso
 rm -rf iso
