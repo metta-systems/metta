736faf025ca3cccff0f2d444ea7d960caa4f6d42
Berkus <berkus@madfire.net>
2009-08-23 21:19:21 +0300

    Add thread context-switch state (as inspired by Fluke).

--- src/vesper/x86/boot_components/scheduler/thread.h
+++ src/vesper/x86/boot_components/scheduler/thread.h
@@ -1,10 +1,29 @@
 #pragma once
 
+#include "page_directory.h"
+
 namespace scheduler
 {
 
 /*!
-Thread is an execution flow abstration, supported by the scheduler.
+* Context-switch state.
+*/
+struct csw_state_t
+{
+    /*!
+    * Page directory to load while running this thread.
+    * Kept up-to-date by the task code in task.cpp.
+    */
+    page_directory_t* page_dir;
+
+    /*!
+    * Saved kernel register state for this thread.
+    */
+    jmp_buf         state;
+};
+
+/*!
+* Thread is an execution flow abstration, supported by the scheduler.
 */
 class thread_t
 {
