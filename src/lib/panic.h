ee48e4bffe572ba8aa6a1474f56f954762c403b7
Berkus <berkus@madfire.net>
2009-08-29 15:42:23 +0300

    panic() does not return.

--- src/lib/panic.h
+++ src/lib/panic.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "types.h"
+#include "macros.h"
 
 #define PANIC(msg) panic(msg, __FILE__, __LINE__)
 
@@ -16,8 +17,8 @@
 #define ASSERT(b) ((b) ? (void)0 : panic_assert(#b, __FILE__, __LINE__))
 #endif
 
-extern "C" void panic(const char* message, const char* file, uint32_t line);
-extern "C" void panic_assert(const char* desc, const char* file, uint32_t line);
+extern "C" void panic(const char* message, const char* file, uint32_t line) NORETURN;
+extern "C" void panic_assert(const char* desc, const char* file, uint32_t line) NORETURN;
 
 // kate: indent-width 4; replace-tabs on;
 // vim: set et sw=4 ts=4 sts=4 cino=(4 :
