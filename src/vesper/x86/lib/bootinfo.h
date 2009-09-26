bc5d6138c949b23716589b766cdf9ccff9cdcf90
Berkus <berkus@madfire.net>
2009-09-26 09:04:31 +0300

    Add bootinfo impl file, use memory_allocator_t instead of boot_pmm_allocator.

--- src/vesper/x86/lib/bootinfo.h
+++ src/vesper/x86/lib/bootinfo.h
@@ -10,22 +10,41 @@
 #include "memory.h"
 #include "memutils.h"
 
+namespace kickstart_n {
+    class memory_allocator_t;
+}
+
 /*!
 * Provide access to boot info page structures.
+*
+* boot info page layout
+* -------------------- START of page
+* 4 bytes info page size
+* 4 bytes flags field - bit 0 means memmgr pointer is valid.
+* x bytes multiboot header
+* y bytes multiboot mmap entries
+* (free space)
+* optional fields:
+* 4 bytes initial memmgr pointer - used to reconstruct paging directories.
+* -------------------- END of page
 */
 class bootinfo_t
 {
 public:
-    bootinfo_t(address_t bootinfo_page) : boot_info(bootinfo_page) {}
+    bootinfo_t(address_t bootinfo_page) : boot_info(bootinfo_page) { flags() = 0; }
     /*!
     * Get size of the boot info page, stored in the first word of the page.
     * You can typically store more info at the end of the page and then
     * call increase_size() to indicate size of the added block.
     */
     size_t size();
+    uint32_t& flags();
+    uint32_t optional_fields_size();
     void increase_size(size_t addend);
     multiboot_t::header_t* multiboot_header();
     bool append_mmap_entry(multiboot_t::mmap_entry_t* entry);
+    kickstart_n::memory_allocator_t* memmgr();
+    bool set_memmgr(kickstart_n::memory_allocator_t*);
 private:
     address_t boot_info;
 };
@@ -35,32 +54,24 @@ inline size_t bootinfo_t::size()
     return *reinterpret_cast<size_t*>(boot_info);
 }
 
+inline uint32_t& bootinfo_t::flags()
+{
+    return *reinterpret_cast<uint32_t*>(boot_info + sizeof(size_t));
+}
+
+inline uint32_t bootinfo_t::optional_fields_size()
+{
+    return 0 + (flags() & 0x1 ? sizeof(address_t) : 0);
+}
+
 inline void bootinfo_t::increase_size(size_t addend)
 {
-    (*reinterpret_cast<size_t*>(boot_info)) += addend;
+    *reinterpret_cast<size_t*>(boot_info) += addend;
 }
 
 inline multiboot_t::header_t* bootinfo_t::multiboot_header()
 {
-    return reinterpret_cast<multiboot_t::header_t*>(boot_info + sizeof(size_t));
-}
-
-//TODO: move to bootinfo.cpp
-inline bool bootinfo_t::append_mmap_entry(multiboot_t::mmap_entry_t* entry)
-{
-    size_t entry_size = sizeof(multiboot_t::mmap_entry_t);
-    if (size() + entry_size > PAGE_SIZE)
-        return false;
-
-    multiboot_t::header_t* header = multiboot_header();
-    uint32_t end = boot_info + size();
-
-    memutils::copy_memory(reinterpret_cast<void*>(end), entry, entry_size);
-    reinterpret_cast<multiboot_t::mmap_entry_t*>(end)->set_entry_size(entry_size); //  ignore any extra fields
-    header->mmap.set_size(header->mmap.size() + entry_size);
-    increase_size(entry_size);
-
-    return true;
+    return reinterpret_cast<multiboot_t::header_t*>(boot_info + sizeof(size_t) + sizeof(uint32_t));
 }
 
 // kate: indent-width 4; replace-tabs on;
