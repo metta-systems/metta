0f7f426eaaeb11a4bb41f15c715b665c3ad5c9fb
user <user@eeek>
2009-10-03 14:46:13 +0300

    New ideas for portal_t impl.

--- src/vesper/arch-x86/nucleus/portal.h
+++ src/vesper/arch-x86/nucleus/portal.h
@@ -10,11 +10,23 @@ namespace nucleus
 {
 
 /*!
-Portal is a way for threads to cross PD borders.
-*/
+ * Portal is a way for threads to cross PD borders.
+ */
+template <const string_t& method_spec>
 class portal_t
 {
-    portal_t();
+public:
+    // create a portal that calls into target_asid at the address
+    // target_loc.
+    portal_t(asid_t target_asid, address_t target_loc);
+
+    // TODO: make template method that expands into series of
+    // append_param() calls with types from method_spec and also
+    // typechecks actual args against formal list in method_spec.
+    void operator()();
+
+    void return_to_caller(); // a paired reverse portal to return from
+    // call to this portal. can also have parameters!
 private:
 };
 
