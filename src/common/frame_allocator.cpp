82df576f0c944146e90b619b39de4ef44ae6baf6
berkus <berkus@chakra>
2009-12-24 04:29:53 +0200

    Check return value of alloc_frame()

--- src/common/frame_allocator.cpp
+++ src/common/frame_allocator.cpp
@@ -6,7 +6,10 @@ void frame_allocator_t::alloc_frame(page_t* p, bool is_kernel, bool is_writeable
     ASSERT(p);
     ASSERT(!p->frame());
 
-    p->set_frame(alloc_frame());
+    address_t frame = alloc_frame();
+    ASSERT(frame); //TODO: handle out of memory more gracefully later.
+    p->set_frame(frame);
+
     p->set_writable(is_writeable);
     p->set_user(!is_kernel);
     p->set_present(true);
