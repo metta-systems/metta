ecbfc77a04e2400020a5e944c43beb408fb8ba98
Berkus <berkus@exquance.com>
2010-01-08 05:55:31 +0200

    Add rbtree_t implementation.

---
+++ src/tests/test_rbtree.cpp
@@ -0,0 +1,69 @@
+//
+// Copyright 2007 - 2009, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+/*!
+ * \brief Test rbtree_t<V>.
+ */
+
+/*============================================================================*/
+
+#include <string.h>
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "rbtree.h"
+
+BOOST_AUTO_TEST_SUITE( test_suite )
+
+template <typename K, typename V>
+struct pair_t
+{
+    typedef pair_t<K,V> self_type;
+
+    K key;
+    V value;
+
+    pair_t(K k, V v) : key(k), value(v) {}
+    bool operator <(self_type other) { return key < other.key; }
+    bool operator <=(self_type other) { return key <= other.key; }
+    bool operator >(self_type other) { return key > other.key; }
+    bool operator >=(self_type other) { return key >= other.key; }
+    bool operator ==(self_type other) { return key == other.key; }
+};
+
+BOOST_AUTO_TEST_CASE(test_rbtree)
+{
+    rbtree_t<int> tree;
+    BOOST_CHECK_EQUAL(tree.fulfills_invariant(), true);
+
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    BOOST_CHECK_EQUAL(tree.fulfills_invariant(), true);
+
+    tree.insert(4);
+    tree.insert(5);
+    BOOST_CHECK_EQUAL(tree.fulfills_invariant(), true);
+
+    typedef pair_t<int,int> int_pair;
+    rbtree_t<int_pair> tree2;
+    BOOST_CHECK_EQUAL(tree2.fulfills_invariant(), true);
+
+    tree2.insert(int_pair(1,2));
+    tree2.insert(int_pair(2,3));
+    tree2.insert(int_pair(3,4));
+    BOOST_CHECK_EQUAL(tree2.fulfills_invariant(), true);
+
+    tree2.insert(int_pair(4,5));
+    tree2.insert(int_pair(5,6));
+    BOOST_CHECK_EQUAL(tree.fulfills_invariant(), true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// kate: indent-width 4; replace-tabs on;
+// vim: set et sw=4 ts=4 sts=4 cino=(4 :
--- src/tests/test_suite_main.cpp
+++ src/tests/test_suite_main.cpp
@@ -4,7 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-/**
+/*!
  * \brief Main entry point.
  */
 
