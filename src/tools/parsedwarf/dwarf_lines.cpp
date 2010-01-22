c47cad438d516640aaeca8c40f7f8ddb9801f347
Berkus <berkus@exquance.com>
2010-01-22 05:35:19 +0200

    Add line program bytecode parsing.

--- src/tools/parsedwarf/dwarf_lines.cpp
+++ src/tools/parsedwarf/dwarf_lines.cpp
@@ -1,8 +1,137 @@
 #include "dwarf_lines.h"
 #include "local_panic.h"
 
-void lineprogram_regs_t::execute(address_t from, size_t& offset)
+bool lineprogram_regs_t::execute(address_t from, size_t& offset)
 {
+    uint8_t opcode = *reinterpret_cast<uint8_t*>(from + offset);
+    ++offset;
+
+    if (reset_to_initial)
+        reset();
+
+    if (reset_fields)
+    {
+        basic_block = false;
+        prologue_end = false;
+        epilogue_begin = false;
+        reset_fields = false;
+    }
+
+    // Decode opcode into standard, extended or special and execute updating current information.
+    if (opcode < header.opcode_base) // std opcode
+    {
+        switch (opcode)
+        {
+            case DW_LNS_copy:
+                reset_fields = true;
+                break;
+            case DW_LNS_advance_pc:
+            {
+                uleb128_t operand;
+                operand.decode(from, offset);
+                address += operand * header.minimum_instruction_length;
+                break;
+            }
+            case DW_LNS_advance_line:
+            {
+                sleb128_t operand;
+                operand.decode(from, offset);
+                line += operand;
+                break;
+            }
+            case DW_LNS_set_file:
+            {
+                uleb128_t operand;
+                operand.decode(from, offset);
+                file = operand;
+                break;
+            }
+            case DW_LNS_set_column:
+            {
+                uleb128_t operand;
+                operand.decode(from, offset);
+                column = operand;
+                break;
+            }
+            case DW_LNS_negate_stmt:
+                is_stmt = !is_stmt;
+                break;
+            case DW_LNS_set_basic_block:
+                basic_block = true;
+                break;
+            case DW_LNS_const_add_pc:
+                address += address_increment(255) * header.minimum_instruction_length;
+                break;
+            case DW_LNS_fixed_advance_pc:
+            {
+                uint16_t operand = *reinterpret_cast<uint16_t*>(from + offset);
+                offset += sizeof(uint16_t);
+                address += operand;
+                break;
+            }
+            case DW_LNS_set_prologue_end:
+                prologue_end = true;
+                break;
+            case DW_LNS_set_epilogue_begin:
+                epilogue_begin = true;
+                break;
+            case DW_LNS_set_isa:
+            {
+                uleb128_t operand;
+                operand.decode(from, offset);
+                isa = operand;
+                break;
+            }
+            case DW_LNS_extended_op:
+            {
+                uleb128_t ext_area_length;
+                ext_area_length.decode(from, offset);
+                size_t prev_offset = offset;
+                uint8_t sub_opcode = *reinterpret_cast<uint8_t*>(from + offset);
+                ++offset;
+                switch (sub_opcode)
+                {
+                    case DW_LNE_end_sequence:
+                        end_sequence = true;
+                        reset_to_initial = true;
+                        break;
+                    case DW_LNE_set_address:
+                    {
+                        uint32_t operand = *reinterpret_cast<uint32_t*>(from + offset);
+                        offset += sizeof(uint32_t);
+                        address = operand;
+                        break;
+                    }
+                    case DW_LNE_define_file:
+                    {
+                        lnp_filename_t fname;
+                        fname.decode(from, offset);
+                        header.file_names.push_back(fname);
+                        break;
+                    }
+                    default:
+                        printf("UNKNOWN EXTENDED OPCODE %x\n", sub_opcode);
+                        return false;
+                }
+                if (offset != prev_offset + ext_area_length)
+                {
+                    printf("STREAM OUT OF SYNC! offset %x should be %x\n", offset, prev_offset + ext_area_length);
+                    return false;
+                }
+            }
+            default:
+                printf("UNKNOWN OPCODE %x\n", opcode);
+                return false;
+        }
+    }
+    else // special opcode
+    {
+        opcode -= header.opcode_base;
+        address += address_increment(opcode);
+        line += line_increment(opcode);
+    }
+
+    return true;
 }
 
 void lnp_header_t::decode(address_t from, size_t& offset)
@@ -47,24 +176,55 @@ void lnp_header_t::decode(address_t from, size_t& offset)
     }
     ++offset;
 
-    char* s = reinterpret_cast<char*>(from + offset);
-    while (*s)
-    {
-        lnp_filename_t fname;
-
-        char* str = s;
-        while (*s++)
-            ++offset;
-        ++offset;
-
-        fname.filename = std::string(str);
-        fname.directory_index.decode(from, offset);
-        fname.file_timestamp.decode(from, offset);
-        fname.file_bytes.decode(from, offset);
-
+    lnp_filename_t fname;
+    while (fname.decode(from, offset))
         file_names.push_back(fname);
-        s = reinterpret_cast<char*>(from + offset);
-    }
-    ++offset;
 }
 
+bool lnp_filename_t::decode(address_t from, size_t& offset)
+{
+    char* s = reinterpret_cast<char*>(from + offset);
+    if (!*s)
+    {
+        ++offset;
+        return false;
+    }
+
+    char* str = s;
+    while (*s++)
+        ++offset;
+    ++offset;
+
+    filename = std::string(str);
+    directory_index.decode(from, offset);
+    file_timestamp.decode(from, offset);
+    file_bytes.decode(from, offset);
+
+    return true;
+}
+
+dwarf_debug_lines_t::dwarf_debug_lines_t(address_t st, size_t sz)
+    : start(st)
+    , size(sz)
+    , current_regs(header)
+{
+    size_t offset = 0;
+    header.decode(start, offset);
+    current_regs.reset();
+
+    while (offset < size)
+    {
+        if (current_regs.execute(start, offset))
+            state_matrix.push_back(current_regs);
+    }
+}
+
+std::string dwarf_debug_lines_t::file_name(int /*index*/)
+{
+    return std::string("");
+}
+
+int dwarf_debug_lines_t::line_number(address_t /*address*/)
+{
+    return 0;
+}
--- src/tools/parsedwarf/dwarf_lines.h
+++ src/tools/parsedwarf/dwarf_lines.h
@@ -2,46 +2,8 @@
 
 #include "types.h"
 #include "leb128.h"
-
-class lineprogram_regs_t
-{
-public:
-    address_t address;
-    int file;
-    int line;
-    int column;
-    bool is_stmt;
-    bool basic_block;
-    bool end_sequence;
-    bool prologue_end;
-    bool epilogue_begin;
-    int isa;
-
-    lineprogram_regs_t(bool stmt)
-        : address(0), file(1), line(1), column(0)
-        , is_stmt(stmt), basic_block(false), end_sequence(false), prologue_end(false), epilogue_begin(false)
-        , isa(0)
-    {
-    }
-
-    // Execute opcode of the line program and update regs.
-    void execute(address_t from, size_t& offset);
-};
-
-enum {
-    DW_LNS_copy = 0x01,
-    DW_LNS_advance_pc = 0x02,
-    DW_LNS_advance_line = 0x03,
-    DW_LNS_set_file = 0x04,
-    DW_LNS_set_column = 0x05,
-    DW_LNS_negate_stmt = 0x06,
-    DW_LNS_set_basic_block = 0x07,
-    DW_LNS_const_add_pc = 0x08,
-    DW_LNS_fixed_advance_pc = 0x09,
-    DW_LNS_set_prologue_end = 0x0a, // dwarf3
-    DW_LNS_set_epilogue_begin = 0x0b, // dwarf3
-    DW_LNS_set_isa = 0x0c // dwarf3
-};
+#include <vector>
+#include <string>
 
 struct lnp_filename_t
 {
@@ -49,11 +11,14 @@ struct lnp_filename_t
     uleb128_t directory_index;
     uleb128_t file_timestamp;
     uleb128_t file_bytes;
+
+    bool decode(address_t from, size_t& offset);
 };
 
 // Line Number Program header.
 class lnp_header_t
 {
+public:
     uint32_t unit_length;
     uint16_t version;
     uint32_t header_length;
@@ -69,16 +34,111 @@ class lnp_header_t
     address_t line_program_start;
 
     void decode(address_t from, size_t& offset);
-}
+};
+
+class lineprogram_regs_t
+{
+public:
+    address_t address;
+    int file;
+    int line;
+    int column;
+    bool is_stmt;
+    bool basic_block;
+    bool end_sequence;
+    bool prologue_end;
+    bool epilogue_begin;
+    int isa;
+    bool reset_fields, reset_to_initial;
+    lnp_header_t& header;
+
+    lineprogram_regs_t(lnp_header_t& h)
+        : address(0), file(1), line(1), column(0)
+        , is_stmt(h.default_is_stmt), basic_block(false), end_sequence(false), prologue_end(false), epilogue_begin(false)
+        , isa(0), reset_fields(false), reset_to_initial(false), header(h)
+    {
+    }
+
+    lineprogram_regs_t& operator=(const lineprogram_regs_t& other)
+    {
+        if (this != &other)
+        {
+            address = other.address;
+            file = other.file;
+            line = other.line;
+            column = other.column;
+            is_stmt = other.is_stmt;
+            basic_block = other.basic_block;
+            end_sequence = other.end_sequence;
+            prologue_end = other.prologue_end;
+            epilogue_begin = other.epilogue_begin;
+            isa = other.isa;
+            reset_fields = other.reset_fields;
+            reset_to_initial = other.reset_to_initial;
+            header = other.header;
+        }
+        return *this;
+    }
+
+    void reset()
+    {
+        address = 0;
+        file = 1;
+        line = 1;
+        column = 0;
+        is_stmt = header.default_is_stmt;
+        basic_block = false;
+        end_sequence = false;
+        prologue_end = false;
+        epilogue_begin = false;
+        isa = 0;
+        reset_to_initial = false;
+    }
+
+    bool execute(address_t from, size_t& offset);
+
+    inline int address_increment(uint8_t opcode)
+    {
+        return (opcode / header.line_range) * header.minimum_instruction_length;
+    }
+
+    inline int line_increment(uint8_t opcode)
+    {
+        return header.line_base + (opcode % header.line_range);
+    }
+};
+
+enum {
+    DW_LNS_extended_op = 0x00,
+    // Standard opcodes
+    DW_LNS_copy = 0x01,
+    DW_LNS_advance_pc = 0x02,
+    DW_LNS_advance_line = 0x03,
+    DW_LNS_set_file = 0x04,
+    DW_LNS_set_column = 0x05,
+    DW_LNS_negate_stmt = 0x06,
+    DW_LNS_set_basic_block = 0x07,
+    DW_LNS_const_add_pc = 0x08,
+    DW_LNS_fixed_advance_pc = 0x09,
+    DW_LNS_set_prologue_end = 0x0a, // dwarf3
+    DW_LNS_set_epilogue_begin = 0x0b, // dwarf3
+    DW_LNS_set_isa = 0x0c, // dwarf3
+    // Extended opcodes
+    DW_LNE_end_sequence = 0x01,
+    DW_LNE_set_address  = 0x02,
+    DW_LNE_define_file  = 0x03
+};
 
 class dwarf_debug_lines_t
 {
-    lnp_header_t* header;
+    address_t start;
+    size_t    size;
+    lnp_header_t header;
     lineprogram_regs_t current_regs;
     std::vector<lineprogram_regs_t> state_matrix;
 
 public:
-    dwarf_debug_lines_t();
+    dwarf_debug_lines_t(address_t st, size_t sz);
 
     std::string file_name(int index);
     int line_number(address_t address);
