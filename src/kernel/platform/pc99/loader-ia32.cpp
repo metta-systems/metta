08e94d1138d75063d52c55386982734a76051947
Berkus <berkus@exquance.com>
2010-05-15 00:42:54 +0300

    Add copyright headers.

--- src/kernel/platform/pc99/loader-ia32.cpp
+++ src/kernel/platform/pc99/loader-ia32.cpp
@@ -1,7 +1,13 @@
-/*!
- * Arch-specific part of the loader.
- */
-
+//
+// Arch-specific part of the loader.
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2010, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include "loader.h"
 
 /*
