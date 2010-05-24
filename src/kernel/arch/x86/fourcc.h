7939f04deeb33aadcc631982f507565f6bfd5a42
Berkus <berkus@exquance.com>
2010-05-25 00:22:40 +0300

    Remove nasty invisible chars.

--- src/kernel/arch/x86/fourcc.h
+++ src/kernel/arch/x86/fourcc.h
@@ -5,5 +5,5 @@
 template <uint8_t a, uint8_t b, uint8_t c, uint8_t d>
 struct FourCC
 {
-    static const uint32_t value = (((((d << 8) | c) << 8) | b) << 8) | a;
+    static const uint32_t value = (((((d << 8) | c) << 8) | b) << 8) | a;
 };
