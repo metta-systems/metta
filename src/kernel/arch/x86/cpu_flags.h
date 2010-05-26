c9487fd71ebf51688a339a601d2ac1f685ac8974
Berkus <berkus@exquance.com>
2010-05-26 16:10:50 +0300

    Add cpu features detection.

---
+++ src/kernel/arch/x86/cpu_flags.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2003-2008,  Karlsruhe University
+ * File path:     arch/x86/x32/x86.h
+ * Description:   IA32 specific constants
+ */
+#pragma once
+
+/**********************************************************************
+ *    CPU features (CPUID)
+ **********************************************************************/
+
+#define X86_32_FEAT_FPU    (1 << 0)
+#define X86_32_FEAT_VME    (1 << 1)
+#define X86_32_FEAT_DE     (1 << 2)
+#define X86_32_FEAT_PSE    (1 << 3)
+#define X86_32_FEAT_TSC    (1 << 4)
+#define X86_32_FEAT_MSR    (1 << 5)
+#define X86_32_FEAT_PAE    (1 << 6)
+#define X86_32_FEAT_MCE    (1 << 7)
+#define X86_32_FEAT_CXS    (1 << 8)
+#define X86_32_FEAT_APIC   (1 << 9)
+#define X86_32_FEAT_SEP    (1 << 11)
+#define X86_32_FEAT_MTRR   (1 << 12)
+#define X86_32_FEAT_PGE    (1 << 13)
+#define X86_32_FEAT_MCA    (1 << 14)
+#define X86_32_FEAT_CMOV   (1 << 15)
+#define X86_32_FEAT_FGPAT  (1 << 16)
+#define X86_32_FEAT_PSE36  (1 << 17)
+#define X86_32_FEAT_PSN    (1 << 18)
+#define X86_32_FEAT_CLFLH  (1 << 19)
+#define X86_32_FEAT_DS     (1 << 21)
+#define X86_32_FEAT_ACPI   (1 << 22)
+#define X86_32_FEAT_MMX    (1 << 23)
+#define X86_32_FEAT_FXSR   (1 << 24)
+#define X86_32_FEAT_XMM    (1 << 25)
+
+/* CPUID.1 ECX */
+#define X86_32_FEAT2_VMX  (1 << 5)
+
+/**********************************************************************
+ *    FLAGS register
+ **********************************************************************/
+
+#define X86_FLAGS_CF      (1 <<  0)       /* carry flag                   */
+#define X86_FLAGS_PF      (1 <<  2)       /* parity flag                  */
+#define X86_FLAGS_AF      (1 <<  4)       /* auxiliary carry flag         */
+#define X86_FLAGS_ZF      (1 <<  6)       /* zero flag                    */
+#define X86_FLAGS_SF      (1 <<  7)       /* sign flag                    */
+#define X86_FLAGS_TF      (1 <<  8)       /* trap flag                    */
+#define X86_FLAGS_IF      (1 <<  9)       /* interrupt enable flag        */
+#define X86_FLAGS_DF      (1 << 10)       /* direction flag               */
+#define X86_FLAGS_OF      (1 << 11)       /* overflow flag                */
+#define X86_FLAGS_NT      (1 << 14)       /* nested task flag             */
+#define X86_FLAGS_RF      (1 << 16)       /* resume flag                  */
+#define X86_FLAGS_VM      (1 << 17)       /* virtual 8086 mode            */
+#define X86_FLAGS_AC      (1 << 18)       /* alignement check             */
+#define X86_FLAGS_VIF     (1 << 19)       /* virtual interrupt flag       */
+#define X86_FLAGS_VIP     (1 << 20)       /* virtual interrupt pending    */
+#define X86_FLAGS_ID      (1 << 21)       /* CPUID flag                   */
+#define X86_FLAGS_IOPL(x) ((x & 3) << 12) /* the IO privilege level field */
--- src/kernel/arch/x86/startup.cpp
+++ src/kernel/arch/x86/startup.cpp
@@ -4,6 +4,7 @@
 // Part of Metta OS. Check http://metta.exquance.com for latest version.
 //
 // Copyright 2007 - 2010, Stanislav Karchebnyy <berkus@exquance.com>
+// Code portions copyright 2002-2008, 2010, Karlsruhe University
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -116,6 +117,45 @@ static void parse_cmdline(bootinfo_t* bi)
 //                  L4_BootLoaderSpecificMemoryType,
 //                  kip_manager_t::desc_boot_module);
 
+static void /*SECTION(".init.cpu")*/ check_cpu_features()
+{
+    uint32_t req_features = X86_32_FEAT_FPU;
+#if CONFIG_X86_PSE
+    req_features |= X86_32_FEAT_PSE;
+#endif
+#if CONFIG_X86_PGE
+    req_features |= X86_32_FEAT_PGE;
+#endif
+#if CONFIG_X86_FXSR
+    req_features |= X86_32_FEAT_FXSR;
+#endif
+#if CONFIG_X86_SYSENTER
+    req_features |= X86_32_FEAT_SEP;
+#endif
+#if CONFIG_IOAPIC
+    req_features |= X86_32_FEAT_APIC;
+#endif
+    uint32_t avail_features = x86_cpu_t::features();
+
+    if ((req_features & avail_features) != req_features)
+    {
+        kconsole << "CPU does not support all required features " << req_features << "(";
+#if defined(CONFIG_VERBOSE_INIT)
+        const char* x86_32_features[] = {
+            "fpu",  "vme",    "de",   "pse",   "tsc",  "msr", "pae",  "mce",
+            "cx8",  "apic",   "?",    "sep",   "mtrr", "pge", "mca",  "cmov",
+            "pat",  "pse-36", "psn",  "cflsh", "?",    "ds",  "acpi", "mmx",
+            "fxsr", "sse",    "sse2", "ss",    "ht",   "tm",  "ia64", "pbe"
+        };
+        for (int i = 0; i < 32; i++)
+            if ((req_features & 1 << i) && (!(avail_features & 1 << i)))
+                kconsole << x86_32_features[i] << " ";
+        kconsole << "missing" << endl;
+#endif
+        PANIC("unsupported CPU!");
+    }
+}
+
 /*!
  * Get the system going.
  *
@@ -133,8 +173,10 @@ void kernel_startup()
     kconsole << "Run global ctors" << endl;
     run_global_ctors();
 
-//     setup_gdt();
-//     setup_idt();
+/*    global_descriptor_table_t gdt;
+    kconsole << "Created gdt." << endl;*/
+/*    interrupt_descriptor_table_t::instance().install();
+    kconsole << "Created idt." << endl;*/
 //     setup_fpu();
 //     setup_pic();
 
@@ -150,40 +192,28 @@ void kernel_startup()
         kconsole << "Nexus at " << start << " till " << end << " named " << name << endl;
     }
 
-    bootimage_t nexus(/*name,*/ start/*, end*/);
+    bootimage_t nexus(name, start, end);
 
     parse_cmdline(bi);
-//     get_cpuid();
+    check_cpu_features();
 //     init_cpu_features();
 //         init_cache();
 //         init_pmctr();
-//     prepare_infopage();
+//     prepare_infopage();  <-- init domain info page
 //     Timer$Enable();
-//     init_mem();
+//     init_mem(); <-- setup gdt and page tables
 //     enable_fpu();
 //     k_presume(RootDomain);
 
 ///    x86_frame_allocator_t::instance().initialise_before_paging(mb.memory_map(), x86_frame_allocator_t::instance().reserved_range());
     // now we can also free dynamic memory
 
-#if MEMORY_DEBUG
-/**    kconsole << GREEN << "lower mem = " << (int)mb.lower_mem() << "KB" << endl
-                      << "upper mem = " << (int)mb.upper_mem() << "KB" << endl;
-
-    kconsole << WHITE << "We are loaded at " << LINKSYM(KICKSTART_BASE) << endl
-                      << "    bootimage at " << bootimage->mod_start << ", end " << bootimage->mod_end << endl;*/
-#endif
 #if 0
     // Identity map currently executing code.
     // page 0 is not mapped to catch null pointers
     map_identity("bottom 4Mb", PAGE_SIZE, 4*MiB - PAGE_SIZE);
 
-    global_descriptor_table_t gdt;
-    kconsole << "Created gdt." << endl;
-
 //     stretch_driver_t::default_driver().initialise();
-    interrupt_descriptor_table_t::instance().install();
-    kconsole << "Created idt." << endl;
 
     static_cast<x86_protection_domain_t&>(protection_domain_t::privileged()).enable_paging();
     // now we have paging enabled.
@@ -221,10 +251,10 @@ void kernel_startup()
 
     // TODO: run a predefined root_server_entry portal here
     kconsole << "Allocating frame: " << frame_allocator_t::instance().allocate_frame() << endl;
+#endif
 
     /* Never reached */
-    PANIC("root_server returned!");
-#endif
+    PANIC("root domain returned!");
 }
 
 // kate: indent-width 4; replace-tabs on;
