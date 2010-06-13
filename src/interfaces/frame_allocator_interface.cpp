4c9c416977d5af53875e009b3a93189e43c5c7b8
Berkus <berkus@exquance.com>
2010-06-13 13:55:28 +0300

    Some stuff not compilable - needs meddler support.

--- src/interfaces/frame_allocator_interface.cpp
+++ src/interfaces/frame_allocator_interface.cpp
@@ -1,7 +1,7 @@
 #include "frame_allocator_interface.h"
 #include "frame_allocator_impl.h"
 
-client_frame_allocator_closure* frame_allocator_closure::new_client()
-{
-    return methods->new_client(this);
-}
+// client_frame_allocator_closure* frame_allocator_closure::new_client()
+// {
+//     return ((frame_allocator_ops*)methods)->new_client(this);
+// }
--- src/interfaces/frame_allocator_interface.h
+++ src/interfaces/frame_allocator_interface.h
@@ -5,7 +5,7 @@
 // frame allocator is a privileged interface that can allocate memory for privileged domain and also create
 // client frame allocators for client (userspace) domains.
 
-DECLARE_CLOSURE_(frame_allocator, client_frame_allocator)
-{
-    client_frame_allocator_closure* new_client();
-};
+// DECLARE_CLOSURE_(frame_allocator, client_frame_allocator)
+// {
+//     client_frame_allocator_closure* new_client();
+// };
