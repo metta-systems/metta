d8cd6b3f875a9c9c3b7bfae1e82f27558579d3e8
Berkus <berkus@exquance.com>
2010-06-18 03:06:21 +0300

    Implement privileged continuation activation.

--- src/kernel/arch/x86/continuation.h
+++ src/kernel/arch/x86/continuation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "types.h"
+#include "segs.h"
 
 // continuation (it records the state of the running computation at the point where it takes off).
 class continuation_t
@@ -8,6 +9,7 @@ class continuation_t
     // continuation flags
     static const int F_CPU_VALID = 1; // CPU context is valid.
     static const int F_FPU_VALID = 2; // FPU context is valid.
+    static const int F_PERV_VALID = 4; // Pervasives pointer is valid.
 
 public:
     struct gpregs_t {
@@ -19,7 +21,12 @@ public:
 
     continuation_t() : flags(0) {}
 
-    void set_entry(uint32_t entry) { regs.eip = entry; }
+    void set_entry(uint32_t entry, uint32_t new_cs = KERNEL_CS, uint32_t new_ds = KERNEL_DS)
+    {
+        regs.eip = entry;
+        cs = new_cs;
+        ds = new_ds;
+    }
     void set_gpregs(gpregs_t reg) { regs = reg; flags |= F_CPU_VALID; }
 
     void activate();
@@ -28,6 +35,30 @@ private:
     gpregs_t regs;
     uint32_t pervasives;
     uint32_t flags;
+    uint32_t cs, ds;
     uint32_t fpregs[27]; // FPU registers
-    uint32_t pad[25]; // use it for e.g. some words on stack too?
+    uint32_t pad[23]; // use it for e.g. some words on stack too?
 };
+
+extern void asm_activate(continuation_t::gpregs_t* gpregs, uint32_t cs, uint32_t ds);
+
+// A privileged method to activate (throw) a continuation.
+// Ideally, the FPU stuff should be initialiazed only if process hits FPU exception by using FPU commands after
+// activation. Saves some switch time.
+void continuation_t::activate()
+{
+    // restore pervasives pointer
+    if (flags & F_PERV_VALID)
+    {
+        INFO_PAGE.pervasives = (void*)pervasives;
+    }
+    // restore FPU registers
+    if (flags & F_FPU_VALID)
+    {
+        asm volatile ("frstor %0" :: "m"(*fpregs));
+    }
+    // restore CPU registers
+    // set cs:ds:es, flags and stack pointer
+    // iret
+    asm_activate(&regs, cs, ds);
+}
---
+++ src/kernel/arch/x86/continuation.s
@@ -0,0 +1,29 @@
+global asm_activate
+
+; void asm_activate(gpregs_t* gpregs, uint32_t cs, uint32_t ds)
+; [ESP+4] = ptr
+asm_activate:
+    mov edi, [esp + 4]  ; gpregs
+    mov ecx, [esp + 8]  ; cs
+    mov edx, [esp + 12] ; ds
+    push edx            ; ss3
+    mov eax, [edi + 36] ; esp3
+    push eax
+    mov eax, [edi + 32] ; eflags
+    push eax
+    push ecx            ; user cs
+    mov eax, [edi + 28] ; eip
+    push eax
+    ; Other regs
+    mov eax, [edi + 12] ; edx
+    push eax            ; current edx contains DS
+    mov eax, [edi + 0] ; eax
+    mov ebx, [edi + 4] ; ebx
+    mov ecx, [edi + 8] ; ecx
+    mov esi, [edi + 16] ; esi
+    mov ebp, [edi + 24] ; ebp
+    mov edi, [edi + 20] ; edi
+    mov ds, dx
+    mov es, dx
+    pop edx
+    iret                ; jump to user mode
