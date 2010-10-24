c96974794005bbd4c30110c71ad731d01690f527
Berkus <berkus@exquance.com>
2010-10-24 19:49:42 +0300

    mkfs.mettafs WIP

--- src/tools/mettafs/block_device.h
+++ src/tools/mettafs/block_device.h
@@ -1,31 +1,28 @@
 #pragma once
 
-//#include <QIODevice>
+#include <string>
+#include <fstream>
 
 /*!
  * The block device emulates a disk block device with configured block size and access times. It uses a regular file in
  * a filesystem to store data.
  */
-class BlockDevice //: public QIODevice
+class block_device_t
 {
-    QString storageFileName;
-    QFile storageFile;
-    blocksize_t blockSize;
-    blockno_t numBlocks;
 public:
-    BlockDevice(const QString& storageFile, bool create = false, blocksize_t blockSize = 0, blockno_t numBlocks = 0);
-    virtual ~BlockDevice();
-
     /*!
      * blockno_t type designates a block address type, main unit of addressing in a block device.
      */
     typedef unsigned long long blockno_t;
     typedef size_t blocksize_t;
 
+    block_device_t(const std::string& storageFile, bool create = false, blocksize_t blockSize = 0, blockno_t numBlocks = 0);
+    virtual ~block_device_t();
+
     /*!
      * Return time in nanoseconds it would take to seek from current block position to seekTo.
      */
-    int seekTime(blockno_t seekTo);
+    int seek_time(blockno_t seekTo);
 
     void seek(blockno_t block);
     blockno_t pos();
@@ -33,8 +30,13 @@ public:
     /*!
      * Read and write functions operate on whole blocks of specific size.
      */
-    QByteArray readBlock(blockno_t block);
-    void writeBlock(blockno_t block, const QByteArray& buffer);
-};
+    blocksize_t read_block(blockno_t block, char* buffer);
+    void write_block(blockno_t block, const char* buffer, blocksize_t bytes);
 
+private:
+    std::string storageFileName;
+    std::fstream* storageFile;
+    blocksize_t blockSize;
+    blockno_t numBlocks;
+};
 
--- src/tools/mettafs/mkfs.cpp
+++ src/tools/mettafs/mkfs.cpp
@@ -1,20 +1,28 @@
-#include "BlockDevice.h"
+#include "block_device.h"
 #include <uuid/uuid.h>
+#include "superblock.h"
+#include "memutils.h"
+#include "fourcc.h"
+#include "macros.h"
 
-int make_fs(BlockDevice& dev, const char* label)
+const int sectorsize = 4096;
+const int nodesize = 4096;
+const int leafsize = 4096;
+
+int make_fs(block_device_t& dev, size_t num_bytes, const char* label)
 {
-    superblock_t super;
+    fs_superblock_t super;
 
     num_bytes = (num_bytes / sectorsize) * sectorsize;
 
-    memset(&super, 0, sizeof(super));
+    memutils::fill_memory(&super, 0, sizeof(super));
 
     super.version = 1;
 //     uint8_t   checksum[CHECKSUM_SIZE];  // [  4] block data checksum
     uuid_generate(super.fsid);
     super.block_offset = 0;             // which block this node is supposed to live in
     super.flags = 0;                    // [ 60] not related to validity, but matches generic header format for different trees.
-    super.magic = MAGIC64BE('M','e','T','T','a','F','S','1');
+    super.magic = Magic64BE<'M','e','T','T','a','F','S','1'>::value;
     super.generation = 1;
 //     uint64_t root;              // [ 84] location of "root of roots" tree
     super.total_bytes = num_bytes;
@@ -25,18 +33,23 @@ int make_fs(BlockDevice& dev, const char* label)
     super.checksum_type = CHECKSUM_TYPE_SHA1;
     super.root_level = 1;
 //     dev_item_t dev_item;        // [123]
-    strncpy(super.label, label, sizeof(super.label));
+    memutils::copy_string(super.label, label, sizeof(super.label));
+
+    UNUSED(dev);
+
+    return 1;
 }
 
 int main(int argc, char** argv)
 {
-    // mkfs deviceName <create 1 or 0>
-    if (argc != 3)
+    // mkfs deviceName <create 1 or 0> <byte size>
+    if (argc != 4)
         return 111;
 
     const char* fname = argv[1];
     int create = atoi(argv[2]);
-    BlockDevice dev(fname, create);
+    size_t size = atoi(argv[3]);
+    block_device_t dev(fname, create);
 
-    make_fs(dev, "test_fs");
+    make_fs(dev, size, "test_fs");
 }
