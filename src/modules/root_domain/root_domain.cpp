1743549adb3fc6524799e61e7b975a50df35ed90
Berkus <berkus@exquance.com>
2010-11-14 02:16:49 +0200

    Break root_domain completely by using module loader from bootinfo page.

--- src/modules/root_domain/root_domain.cpp
+++ src/modules/root_domain/root_domain.cpp
@@ -7,10 +7,12 @@
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include "bootimage.h"
+#include "bootinfo.h"
 #include "root_domain.h"
 #include "panic.h"
 #include "default_console.h"
 #include "debugger.h"
+#include "module_loader.h"
 
 root_domain_t::root_domain_t(bootimage_t& img)
     : ns(0, 0)
@@ -20,14 +22,20 @@ root_domain_t::root_domain_t(bootimage_t& img)
     elf.parse(mi.start);
     if (!elf.is_valid())
         PANIC("invalid root_domain elf image");
+
+    bootinfo_t* bi = new(BOOTINFO_PAGE) bootinfo_t;
+    bi->get_module_loader().load_module("root_domain", elf, "entry");///******************************************
+    mi.start = 16*MiB;
+
     elf32::section_header_t* text = elf.section_header(".text");
     address_t entry = elf.get_entry_point();
     ptrdiff_t offset = mi.start - text->addr + text->offset;
     if (!elf.is_relocatable() && offset != 0)
         PANIC("non-relocatable root domain");
-    elf.relocate_to(mi.start);
+    elf.relocate_to(mi.start, 0);
     kconsole << "Root domain relocated by " << (unsigned)offset << endl;
     entry_point = entry + offset;
+
     bochs_magic_trap();
 }
 
