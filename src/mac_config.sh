c48cca18edf71549ca3c6107b541f0de4c496f52
Berkus <berkus@madfire.net>
2011-03-20 21:15:56 +0200

    Mac switch: don't build floppy (no grub binary), don't build tests yet, separate host and target compiler (using a helper setup script mac_config.sh), use mac bochs.

---
+++ src/mac_config.sh
@@ -0,0 +1,1 @@
+CPP=/usr/local/bin/i686-elf-cpp AR=/usr/local/bin/i686-elf-ar RANLIB=/usr/local/bin/i686-elf-ranlib CC=/usr/local/bin/i686-elf-gcc CXX=/usr/local/bin/i686-elf-g++ ./waf configure --clang=no
