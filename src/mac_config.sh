4c2602e177dda61ea3d00aa3f80223aa07543129
Berkus <berkus@madfire.net>
2011-04-03 11:16:20 +0300

    Add clang version to config script (hardcoded).

--- src/mac_config.sh
+++ src/mac_config.sh
@@ -1,1 +1,1 @@
-CPP=/usr/local/bin/i686-elf-cpp AR=/usr/local/bin/i686-elf-ar RANLIB=/usr/local/bin/i686-elf-ranlib CC=/usr/local/bin/i686-elf-gcc CXX=/usr/local/bin/i686-elf-g++ ./waf configure --clang=no
+CLANG=clang-2.9 CPP=/usr/local/bin/i686-elf-cpp AR=/usr/local/bin/i686-elf-ar RANLIB=/usr/local/bin/i686-elf-ranlib CC=/usr/local/bin/i686-elf-gcc CXX=/usr/local/bin/i686-elf-g++ ./waf configure --clang=yes
