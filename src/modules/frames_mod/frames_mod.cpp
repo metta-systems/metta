e60017ef39c79f462bffecc51c825c8765525e00
Berkus <berkus@exquance.com>
2011-06-03 23:50:05 +0300

    frames_mod init in progress, still issues with physmem map.

--- src/modules/frames_mod/frames_mod.cpp
+++ src/modules/frames_mod/frames_mod.cpp
@@ -5,12 +5,13 @@
 #include "default_console.h"
 #include "bootinfo.h"
 #include "algorithm"
+#include "system_frame_allocator_v1_impl.h"
 
 struct frames_client_state
 {
     frame_allocator_v1_closure closure;
     
-    uint32_t n_phys_frames;
+    uint32_t n_allocated_phys_frames;
 
     uint32_t owner;
     size_t guaranteed_frames;
@@ -37,6 +38,20 @@ struct frames_module_v1_state
 };
 
 //======================================================================================================================
+// system_frame_allocator_v1 implementation
+//======================================================================================================================
+
+static const system_frame_allocator_v1_ops system_frame_allocator_v1_methods = {
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+//======================================================================================================================
 // frames_module_v1 implementation
 //======================================================================================================================
 
@@ -55,6 +70,8 @@ static memory_v1_size frames_module_v1_required_size(frames_module_v1_closure* s
 
     res = sizeof(frame_allocator_v1_closure) + sizeof(frames_client_state) + n_regions * sizeof(frames_module_v1_state) + n_frames * sizeof(frame_st);
     res = page_align_up(res);
+    
+    kconsole << " +-frames_mod: counted " << n_regions << " memory regions" << endl;
 /*
     res = sizeof(Frames_cl) + sizeof(FramesClient_st) + 
 	(nreg * sizeof(struct _FramesMod_st)) +
@@ -63,36 +80,69 @@ static memory_v1_size frames_module_v1_required_size(frames_module_v1_closure* s
     return res;
 }
 
-//Don't need where, bootinfo page has logic for finding an allocatable place. We use it currently because MMU mod has to enter some initial mappings - but this can be streamlined.
+//Don't need where, bootinfo page has logic for finding an allocatable place. We use it currently because MMU mod has to enter some initial mappings - but this could be streamlined.
+
+//temp dup from mmu_mod
+#define OWNER_SYSTEM  0x1     /* pfn is owned by us (mmgmt etc) */
 
 static system_frame_allocator_v1_closure* frames_module_v1_create(frames_module_v1_closure* self, ramtab_v1_closure* rtab, memory_v1_address where_to_start)
 {
     UNUSED(self);
-/*    bootinfo_t* bi = new(BOOTINFO_PAGE) bootinfo_t; // simplify memory map operations
-    
-	address_t first_range = bi->find_highmem_range_of_at_least(required_size());
-
-    // Find proper location to start "allocating" from.
-	first_range = page_align_up(first_range);
-
-	if (!bi->use_memory(first_range, mmu_memory_needed_bytes))
-	{
-        PANIC("Unable to use memory for initial frames_mod setup!");
-	}*/
-
-//     std::for_each(bi->mmap_begin(), bi->mmap_end(), [](const multiboot_t::mmap_entry_t e)
-//     {
-//         kconsole << "mmap entry @ " << e.address() << " is " << e.size() << " bytes of type " << e.type() << endl;
-//         allmem.push_back(PMemDesc(e.address(), e.size(), e.type()));
-//     });
-//     std::for_each(bi->vmap_begin(), bi->vmap_end(), [](const memory_v1_mapping* e)
-//     {
-//         kconsole << "vmap entry @ " << e.address() << " is " << e.size() << " bytes of type " << e.type() << endl;
-//         memmap.push_back(PMemDesc(e.address(), e.size(), e.type()));
-//     });
     
 	kconsole << "frames_mod create @ " << where_to_start << endl;
-	return 0;
+    frames_client_state* client_state = reinterpret_cast<frames_client_state*>(where_to_start);
+
+    system_frame_allocator_v1_closure* ret = reinterpret_cast<system_frame_allocator_v1_closure*>(&client_state->closure);
+    ret->state = reinterpret_cast<system_frame_allocator_v1_state*>(client_state);
+    ret->methods = &system_frame_allocator_v1_methods;
+
+    frames_module_v1_state* frames_state = reinterpret_cast<frames_module_v1_state*>(where_to_start + sizeof(frames_client_state));
+    
+    client_state->owner = OWNER_SYSTEM;
+    client_state->n_allocated_phys_frames = 0;
+    client_state->guaranteed_frames = -1;
+    client_state->extra_frames = -1;
+    client_state->heap = 0;
+    client_state->module_state = frames_state;
+    
+    frames_module_v1_state* running_state = frames_state;
+    frames_module_v1_state* last_state = running_state;
+    size_t n_regions = 0;
+
+    bootinfo_t* bi = new(BOOTINFO_PAGE) bootinfo_t;
+    std::for_each(bi->mmap_begin(), bi->mmap_end(), [&running_state, &last_state, &n_regions, rtab](const multiboot_t::mmap_entry_t* e)
+    {
+        running_state->start = e->address();
+        running_state->n_logical_frames = e->size() >> FRAME_WIDTH;
+        running_state->frame_width = FRAME_WIDTH;
+// temp defines
+#define RAM_FREE 1
+#define ACPI_RECLAIMABLE 3
+        if ((e->type() == RAM_FREE) || (e->type() == ACPI_RECLAIMABLE))
+        {
+            running_state->attrs = memory_v1_attrs_regular;
+            running_state->ramtab = rtab;
+        }
+        else
+        {
+            running_state->attrs = memory_v1_attrs_non_memory;
+            running_state->ramtab = 0;            
+        }
+        running_state->frames = reinterpret_cast<frame_st*>(running_state + 1);
+        
+        for(size_t j = 0; j < running_state->n_logical_frames; ++j) 
+    	    running_state->frames[j].free = running_state->n_logical_frames - j;
+
+        running_state->next = reinterpret_cast<frames_module_v1_state*>(&running_state->frames[running_state->n_logical_frames]);
+        last_state = running_state;
+        running_state = running_state->next;
+        ++n_regions;
+    });
+    last_state->next = 0;
+
+    kconsole << " +-frames_mod: counted " << n_regions << " memory regions again" << endl;
+    
+	return ret;
 }
 
 static void frames_module_v1_finish_init(frames_module_v1_closure* self, system_frame_allocator_v1_closure* frames, heap_v1_closure* heap)
