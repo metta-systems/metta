15b4f82b2b061f6e1854574db83ed4e8e203be1b
Berkus <berkus@exquance.com>
2011-06-15 00:21:51 +0300

    Implement dl_link_t, rename frames_mod method.

--- src/kernel/arch/x86/domain.h
+++ src/kernel/arch/x86/domain.h
@@ -26,10 +26,64 @@ DCB is taken mostly verbatim from Nemesis, here's the original diagram:
 */
 
 // Just a doubly linked list, plain and simple.
-struct flink_t
+struct dl_link_t
 {
-    flink_t* next;
-    flink_t* prev;
+    dl_link_t* next;
+    dl_link_t* prev;
+    
+    inline void init()
+    {
+        next = prev = this;
+    }
+    
+    inline bool is_empty()
+    {
+        return next == this;
+    }
+    
+    inline void remove()
+    {
+        next->prev = prev;
+        prev->next = next;
+    }
+    
+    /*!
+     * Insert another before this.
+     */
+    inline void insert_before(dl_link_t* another)
+    {
+        another->next = this;
+        another->prev = prev;
+        prev->next = another;
+        prev = another;
+    }
+    
+    /*!
+     * Insert another after this.
+     */
+    inline void insert_after(dl_link_t* another)
+    {
+        another->next = next;
+        another->prev = this;
+        next->prev = another;
+        next = another;
+    }
+/*    
+    inline dl_link_t* dequeue()
+    {
+        if (is_empty())
+            return NULL;
+        next->remove();
+        return next;
+    }
+*/
+};
+
+struct region_list_t : public dl_link_t
+{
+    address_t  start;         /* Start of physical memory region         */
+    size_t     n_phys_frames; /* No of *physical* frames it extends      */
+    size_t     frame_width;   /* Logical frame width within region       */
 };
 
 struct dcb_rw_t;
@@ -41,7 +95,7 @@ struct dcb_ro_t
     uint32_t min_phys_frame_count;
     uint32_t max_phys_frame_count;
     ramtab_entry_t* ramtab;
-    flink_t memory_region_list;
+    dl_link_t memory_region_list;
 };
 
 struct dcb_rw_t
