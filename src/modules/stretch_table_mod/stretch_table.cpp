93637a60d23f4f8b52876e49564c0140acfdada0
Berkus <berkus@exquance.com>
2011-06-16 04:25:30 +0300

    Add stretch_table impl dummy.

---
+++ src/modules/stretch_table_mod/stretch_table.cpp
@@ -0,0 +1,5 @@
+/*get(stretch_v1& stretch, out card32 page_width, out stretch_driver_v1& stretch_driver) returns (boolean result);
+put(stretch_v1& stretch, card32 page_width, stretch_driver_v1& stretch_driver) returns (boolean result);
+remove(stretch_v1& stretch, out card32 page_width, out stretch_driver_v1& stretch_driver) returns (boolean result);
+destroy();
+*/
