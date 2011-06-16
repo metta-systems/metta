02bad75f759ae2fb4e1269fc9b8dbd02257a7fb1
Berkus <berkus@exquance.com>
2011-06-16 04:17:32 +0300

    Add impl for stretch_tables.

--- src/modules/stretch_table_mod/stretch_table_mod.cpp
+++ src/modules/stretch_table_mod/stretch_table_mod.cpp
@@ -1,8 +1,17 @@
 #include "stretch_table_module_v1_interface.h"
 #include "stretch_table_module_v1_impl.h"
 
+static stretch_table_v1_closure* create(stretch_table_module_v1_closure* self, heap_v1_closure* heap)
+{
+    return 0;
+}
+
+static const stretch_table_module_v1_ops ops = {
+    create
+};
+
 static const stretch_table_module_v1_closure clos = {
-    NULL,
+    &ops,
     NULL
 };
 
