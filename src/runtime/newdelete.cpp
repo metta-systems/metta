58b58f05e702e35ade36f31f51edbd451350128e
Berkus <berkus@exquance.com>
2011-06-18 06:07:48 +0300

    Clean up runtime code.

--- src/runtime/newdelete.cpp
+++ src/runtime/newdelete.cpp
@@ -12,77 +12,8 @@
 #include "macros.h"
 #include "memory.h"
 #include "memutils.h"
-// #include "frame_allocator.h"
 #include "default_console.h"
 #include "debugger.h"
-
-static inline void* placement_alloc(size_t size)
-{
-    kconsole << " .normal frame alloc. ";
-    UNUSED(size);
-    return (void*)0xc000;
-//     return reinterpret_cast<void*>(frame_allocator_t::instance().allocate_frame());
-}
-
-// Allocate small non-page-aligned objects from a preallocated page,
-// switching to a new page as necessary.
-static inline void* small_alloc(size_t size)
-{
-    kconsole << " .small alloc. ";
-    ASSERT(size < PAGE_SIZE/4);
-    static address_t alloc_page = 0;
-    static size_t alloc_pos = 0;
-
-    if (!alloc_page || (alloc_pos + size > PAGE_SIZE))
-    {
-        alloc_page = reinterpret_cast<address_t>(placement_alloc(PAGE_SIZE));
-        alloc_pos = 0;
-    }
-
-    address_t tmp = alloc_page + alloc_pos;
-    alloc_pos += (size + 3) & ~3;
-    return (void*)tmp;
-}
-
-void* operator new(size_t size, bool page_align, address_t* addr)
-{
-    kconsole << RED << "operator new(" << (int)size << ", " << (int)page_align << ", " << (address_t)addr << ")";
-    void* tmp;
-
-    if (!page_align && (size < PAGE_SIZE/4))
-        tmp = small_alloc(size);
-    else
-        tmp = placement_alloc(size);
-
-    if (addr)
-        *addr = (address_t)tmp;
-
-    kconsole << LIGHTRED << "return " << (address_t)tmp << endl;
-    return tmp;
-}
-
-void* operator new(size_t size)
-{
-    return operator new(size, false, NULL);
-}
-
-void* operator new[](size_t size)
-{
-    return operator new(size, false, NULL);
-}
-
-void* operator new[](size_t size, bool page_align, address_t* addr)
-{
-    return operator new(size, page_align, addr);
-}
-
-void operator delete(void*)
-{
-}
-
-void operator delete[](void*)
-{
-}
 
 // Compliant functions for STL allocator (TODO: replace stl malloc_alloc with other default allocator)
 extern "C" void* malloc(size_t size)
@@ -93,13 +24,19 @@ extern "C" void* malloc(size_t size)
     return operator new(size, false, NULL);
 }
 
-extern "C" void free(void*)
+extern "C" void free(void*p)
 {
+    kconsole << RED << "free(" << p << ") -> ";
+    debugger_t::print_backtrace();
+    halt();
 }
 
 // we assume that stl uses realloc only to grow storage
 extern "C" void *realloc(void *ptr, size_t size)
 {
+    kconsole << RED << "realloc(" << ptr << ", " << size << ") -> ";
+    debugger_t::print_backtrace();
+    halt();
     void* ptr2 = malloc(size);
     memutils::copy_memory(ptr2, ptr, size); // may fail since new size is assumed to be larger than the old.
     free(ptr);
