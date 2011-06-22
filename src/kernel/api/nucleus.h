167c66fe5ddb0c1b15b7d0f5c736d27ff687f14b
Berkus <berkus@exquance.com>
2011-06-22 04:37:33 +0300

    Simulate syscalls by directly calling necessary functions for now.

--- src/kernel/api/nucleus.h
+++ src/kernel/api/nucleus.h
@@ -1,15 +1,26 @@
-// nucleus wraps up syscall interface for clients.
+//
+// Part of Metta OS. Check http://metta.exquance.com for latest version.
+//
+// Copyright 2007 - 2011, Stanislav Karchebnyy <berkus@exquance.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Nucleus wraps up syscall interface for clients.
+//
 #include "macros.h"
+#include "mmu.h"
 
 namespace nucleus
 {
     template <typename T1, typename T2>
     inline void syscall(int syscall_no, T1 arg1, T2 arg2)
     {
-        UNUSED(syscall_no);
-        UNUSED(arg1);
         UNUSED(arg2);
-        PANIC("Syscall unimplemented!");
+        if (syscall_no == 0x80)
+            ia32_mmu_t::set_active_pagetable(arg1);
+        else
+            PANIC("Syscall unimplemented!");
     }
 
     //==================================================================================================================
