1354832bfb36b77e2dc5a06a4a1577d21712d362
Berkus <berkus@exquance.com>
2011-06-22 04:42:09 +0300

    Fix operator new() declarations.

--- src/modules/heap_new.h
+++ src/modules/heap_new.h
@@ -2,7 +2,7 @@
 
 #include "heap_v1_interface.h"
 
-void* operator new(size_t size, heap_v1_closure* heap);
-void* operator new[](size_t size, heap_v1_closure* heap);
+void* operator new(size_t size, heap_v1_closure* heap) throw();
+void* operator new[](size_t size, heap_v1_closure* heap) throw();
 void operator delete(void* p, heap_v1_closure* heap) throw();
 void operator delete[](void* p, heap_v1_closure* heap) throw();
