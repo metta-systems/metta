9ad6af66a1483a94617bb1d74d873209f8a16700
Berkus <berkus@exquance.com>
2011-07-04 03:58:30 +0300

    Boilerplate text update.

--- src/kernel/platform/pc99/loader-multiboot.cpp
+++ src/kernel/platform/pc99/loader-multiboot.cpp
@@ -1,12 +1,12 @@
 //
-// Loader version that supports multiboot specification.
-//
 // Part of Metta OS. Check http://metta.exquance.com for latest version.
 //
-// Copyright 2007 - 2010, Stanislav Karchebnyy <berkus@exquance.com>
+// Copyright 2007 - 2011, Stanislav Karchebnyy <berkus@exquance.com>
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Loader version that supports multiboot specification.
 //
 #include "multiboot.h"
 #include "bootinfo.h"
@@ -78,3 +78,6 @@ address_t mbi_init()
 
     return (address_t)bi->get_module_loader().load_module("kernel_boot", elf, NULL);
 }
+
+// kate: indent-width 4; replace-tabs on;
+// vim: set et sw=4 ts=4 sts=4 cino=(4 :
