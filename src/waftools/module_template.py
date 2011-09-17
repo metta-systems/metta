fa18b93490b17bf598a1184a9876a05a1d1f9e5c
Berkus <berkus@exquance.com>
2011-09-17 19:25:56 +0300

    Buildable for pc99 platform.

--- src/waftools/module_template.py
+++ src/waftools/module_template.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import Utils
+import Utils, Options
 
 def set_options(opt):
     pass
 
-def setup_module_build(bld, name):
+def setup_module_build(bld, name, prefix):
+    if prefix: prefix = prefix + '/'
+    arch = Options.options.arch
+    platform = Options.options.platform
     mod = bld.new_task_gen('cxx', 'program')
     mod.target = name+'.comp'
     mod.env = bld.env_of_name('KERNEL_ENV').copy()
     mod.env.append_unique('LINKFLAGS', ['-Wl,-r']); # Components are relocatable
     mod.env.append_unique('LINKFLAGS', ['-T', '../modules/component.lds', '-Wl,-Map,'+name+'.map'])
-    mod.includes = '. ../../runtime ../../runtime/stl ../../interfaces ../../kernel/api ../../kernel/generic ../../kernel/arch/x86 ../../kernel/platform/pc99 ../../modules'
+    mod.includes = ['.', prefix+'../runtime', prefix+'../runtime/stl', prefix+'../interfaces', prefix+'../kernel/api', prefix+'../kernel/generic', prefix+'../kernel/arch/'+arch, prefix+'../kernel/platform/'+platform, prefix]
     mod.uselib_local = 'component_support interfaces kernel platform common runtime'
+    mod.idl_source = 'all_idl' # Trigger building interface files.
     bld.new_task_gen(
         source = name+'.comp',
         rule = 'nm -u ${SRC[0].abspath(env)}',
