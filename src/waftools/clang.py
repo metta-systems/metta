a7a7e163205e88fba1a28f398785d1ef8a95dfe5
Berkus <berkus@exquance.com>
2012-03-05 09:49:23 +0200

    Switching to new waf 1.6.11.

--- src/waftools/clang.py
+++ src/waftools/clang.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os,sys
-import Configure,Options,Utils
-import ccroot,ar
-from Configure import conftest
+from waflib.Tools import ccroot, ar
+from waflib import Utils,Configure,Options
+from waflib.Configure import conf
+@conf
 def find_clang(conf):
     v=conf.env
     clang=None
@@ -14,48 +15,45 @@ def find_clang(conf):
     if not clang:clang=conf.find_program('clang++',var='CLANG')
     if not clang:conf.fatal('clang was not found')
     clang=conf.cmd_to_list(clang)
-    ccroot.get_cc_version(conf,clang)
     v['CXX_NAME']='clang'
     v['CXX']=clang
     v['COMPILER_CXX']=clang
     v['CC_NAME']='clang'
     v['CC']=clang
     v['COMPILER_CC']=clang
+@conf
 def clang_common_flags(conf):
     v=conf.env
-    v['CXX_SRC_F']=''
+    v['CXX_SRC_F']=[]
     v['CXX_TGT_F']=['-c','-o','']
-    v['CPPPATH_ST']='-I%s'
     if not v['LINK_CXX']:v['LINK_CXX']=v['CXX']
     if not v['LINK_CC']:v['LINK_CC']=v['CC']
-    v['CXXLNK_SRC_F']=''
+    v['CXXLNK_SRC_F']=[]
     v['CXXLNK_TGT_F']=['-o','']
+    v['CPPPATH_ST']='-I%s'
+    v['DEFINES_ST']='-D%s'
     v['LIB_ST']='-l%s'
     v['LIBPATH_ST']='-L%s'
-    v['STATICLIB_ST']='-l%s'
-    v['STATICLIBPATH_ST']='-L%s'
+    v['STLIB_ST']='-l%s'
+    v['STLIBPATH_ST']='-L%s'
     v['RPATH_ST']='-Wl,-rpath,%s'
-    v['CXXDEFINES_ST']='-D%s'
     v['SONAME_ST']='-Wl,-h,%s'
-    v['SHLIB_MARKER']=''
-    v['STATICLIB_MARKER']=''
-    v['FULLSTATIC_MARKER']='-static'
-    v['program_PATTERN']='%s'
-    v['shlib_CXXFLAGS']=['-fPIC','-DPIC']
-    v['shlib_LINKFLAGS']=['-shared']
-    v['shlib_PATTERN']='lib%s.so'
-    v['staticlib_LINKFLAGS']=['']
-    v['staticlib_PATTERN']='lib%s.a'
+    v['SHLIB_MARKER']=''#'-Wl,-Bdynamic'
+    v['STLIB_MARKER']=''#'-Wl,-Bstatic'
+    v['cxxprogram_PATTERN']='%s'
+    v['CXXFLAGS_cxxshlib']=['-fPIC']
+    v['LINKFLAGS_cxxshlib']=['-shared']
+    v['cxxshlib_PATTERN']='lib%s.so'
+    v['LINKFLAGS_cxxstlib']=['-Wl,-Bstatic']
+    v['cxxstlib_PATTERN']='lib%s.a'
     v['LINKFLAGS_MACBUNDLE']=['-bundle','-undefined','dynamic_lookup']
-    v['CCFLAGS_MACBUNDLE']=['-fPIC']
+    v['CXXFLAGS_MACBUNDLE']=['-fPIC']
     v['macbundle_PATTERN']='%s.bundle'
-def detect(conf):
+def configure(conf):
     conf.find_clang()
-    #conf.find_cpp()
-    #conf.find_ar()
     conf.clang_common_flags()
     conf.cxx_load_tools()
     conf.cxx_add_flags()
 
-conftest(find_clang)
-conftest(clang_common_flags)
+# conftest(find_clang)
+# conftest(clang_common_flags)
---
+++ src/waftools/meddle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Convert interface file into set of header and implementation files.
+# name_vX.if -> name_vX_interface.h
+#            -> name_vX_interface.cpp
+#            -> name_vX_impl.h
+#
+from waflib.Task import Task
+from waflib.TaskGen import extension
+
+# First input is the name of meddler executable, as prepared by process_src below.
+class meddle(Task):
+    color = 'CYAN'
+    verbose = False
+    def run(self):
+        cwd = self.outputs[0].parent.bldpath()
+        incd = [self.inputs[1].parent.bldpath()]
+        print "includes:"
+        for i in self.env.IDL_INC:
+            print "include "+i
+            incd = incd + [i]
+
+        if (self.verbose):
+            cmd = '%s -v %s -o%s -I%s' % (self.inputs[0].abspath(), self.inputs[1].abspath(), cwd, ' -I'.join(incd))
+        else:
+            cmd = '%s %s -o%s -I%s' % (self.inputs[0].abspath(), self.inputs[1].abspath(), cwd, ' -I'.join(incd))
+
+        print "Running "+cmd
+        return self.exec_command(cmd)
+
+@extension('.if')
+def process_src(self, node):
+	tg = self.bld.get_tgen_by_name('meddler')
+	comp = tg.tasks[-1].outputs[0]
+	tsk = self.create_task('meddle', [comp, node], 
+		[node.change_ext('_interface.cpp'), node.change_ext('_interface.h'), node.change_ext('_impl.h')]) # Use node.name.replace instead of change_ext?
+	self.source.extend(tsk.outputs)
+
+# Added _interface.h and _impl.h files need not be processed.
+@extension('.h')
+def foo(*k, **kw):
+	pass
