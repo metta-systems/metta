cb9a92ff5e292189058983568ba444066f6af887
Berkus <berkus@exquance.com>
2012-03-24 17:25:57 +0200

    Linking cleanup for clang.

--- src/waftools/module_template.py
+++ src/waftools/module_template.py
@@ -12,21 +12,24 @@ def options(opt):
 @conf
 def undef_check(bld, filename):
     bld(
-        source = filename,
-        rule = bld.env['NM']+' -u ${SRC[0].abspath(env)}',
+        source = bld.path.find_or_declare(filename),
+        rule = '%s -u ${SRC[0].abspath()}' % bld.env['NM'],
         name = 'nm undef check ('+filename+')',
-        after = 'cxx_link'
+        after = 'init.img'
     )
 
 @conf
-def setup_module_build(bld, name, prefix):
+def setup_module_build(bld, name, prefix, sources):
     if prefix: prefix = prefix + '/'
     arch = Options.options.arch
     platform = Options.options.platform
 
-    mod = bld(features='cxx')
-    mod.target = name+'.comp'
-    mod.env = bld.all_envs['KERNEL_ENV'].derive()
+    mod = bld.program(
+        source = sources,
+        target = name+'.comp',
+        use = 'component_support interfaces kernel platform common runtime debug',
+        env = bld.all_envs['KERNEL_ENV'].derive()
+    )
     incs = []
     for x in mod.env.INCLUDE_DIRS:
         if os.path.isabs(x):
@@ -34,15 +37,11 @@ def setup_module_build(bld, name, prefix):
         else:
             incs.append(prefix+'../'+x)
     mod.includes = incs + ['.', prefix]
-    # mod.includes = ['.', prefix+'../runtime', prefix+'../runtime/stl', prefix+'../interfaces', prefix+'../kernel/api', prefix+'../kernel/generic', prefix+'../kernel/arch/'+arch, prefix+'../kernel/platform/'+platform, prefix+'../kernel/arch/shared', prefix+'../kernel/platform/shared', prefix]
-    mod.use = 'component_support interfaces kernel platform common runtime debug'
     mod.env.append_unique('LINKFLAGS', ['-Wl,-r']); # Components are relocatable
     if platform != 'hosted':
+        # mod.env.append_unique('LINKFLAGS', ['-T', '../modules/component.lds'])
         mod.env.append_unique('LINKFLAGS', ['-T', '../modules/component.lds', '-Wl,-Map,'+name+'.map'])
     # bld.undef_check(name+'.comp')
+    print "***** mod features", mod.features 
     bld.all_task_gen += [mod]
     return mod
-
-# Utils.g_module.__dict__['undef_check'] = undef_check
-# Utils.g_module.__dict__['setup_module_build'] = setup_module_build
-
