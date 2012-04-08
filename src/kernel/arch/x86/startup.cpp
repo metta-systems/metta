71543e6477017a82687f180f82d781087568059e
Berkus <berkus@exquance.com>
2012-04-08 15:33:48 +0300

    Factor out nucleus-related code.

--- src/kernel/arch/x86/startup.cpp
+++ src/kernel/arch/x86/startup.cpp
@@ -19,13 +19,9 @@
 #include "continuation.h"
 #include "cpu.h"
 #include "c++ctors.h"
-#include "gdt.h"
-#include "idt.h"
-#include "isr.h"
 #include "root_domain.h"
 #include "registers.h"
 #include "new.h"
-#include "debugger.h"
 
 static void parse_cmdline(bootinfo_t* bi)
 {
@@ -201,67 +197,7 @@ static void prepare_infopage()
 
 extern timer_v1::closure_t* init_timer(); // YIKES external declaration! FIXME
 static continuation_t new_context;
-
-static void dump_regs(registers_t* regs)
-{
-    kconsole << endl << RED 
-        << "=================================================================================================" << endl
-        << "Interrupt " << regs->int_no << ", error code " << regs->err_code << endl
-        << "     EAX:" << regs->eax << " EBX:" << regs->ebx << " ECX:" << regs->ecx << " EDX:" << regs->edx << endl
-        << "     ESI:" << regs->esi << " EDI:" << regs->edi << " EBP:" << regs->ebp << " ESP:" << regs->esp << endl
-        << "user ESP:" << regs->useresp << " CS:" << regs->cs << " DS:" << regs->ds << " SS:" << regs->ss << endl
-        << "     EIP:" << regs->eip << " EFLAGS:" << regs->eflags;
-
-    // EFLAGS bits names from msb (bit 31) to lsb (bit 0)
-    static const char* eflags_bits[] = {
-        "<31>", "<30>", "<29>", "<28>", "<27>", "<26>", "<25>", "<24>",
-        "<23>", "<22>", "ID", "VIP", "VIF", "AC", "VM", "RF",
-        "<15>", "NT", "IOPL1", "IOPL0", "OF", "DF", "IF", "TF",
-        "SF", "ZF", "<5>", "AF", "<3>", "PF", "<1>", "CF"
-    };
-    for (int i = 0; i < 32; i++)
-        if (regs->eflags & (1 << (31 - i)))
-            kconsole << " " << eflags_bits[i];
-
-    kconsole << endl;
-    debugger_t::print_backtrace(regs->ebp, regs->eip, 20);
-
-    kconsole << "=================================================================================================" << endl;    
-}
-
-class general_fault_handler_t : public interrupt_service_routine_t
-{
-public:
-    virtual void run(registers_t* regs)
-    {
-        dump_regs(regs);
-        PANIC("GENERAL PROTECTION FAULT");
-    }
-};
-
-class invalid_opcode_handler_t : public interrupt_service_routine_t
-{
-public:
-    virtual void run(registers_t* regs)
-    {
-        dump_regs(regs);
-        PANIC("INVALID OPCODE");
-    }
-};
-
-class dummy_handler_t : public interrupt_service_routine_t
-{
-public:
-    virtual void run(registers_t* regs)
-    {
-        dump_regs(regs);
-        PANIC("CATCH-ALL");
-    }
-};
-
-general_fault_handler_t gpf_handler;
-invalid_opcode_handler_t iop_handler;
-dummy_handler_t all_exceptions_handler;
+extern "C" void nucleus_init();
 
 /*!
  * Get the system going.
@@ -275,43 +211,7 @@ extern "C" void kernel_startup()
     // No dynamic memory allocation here yet, global objects not constructed either.
     run_global_ctors();
 
-// TODO: this goes into nucleus .init.code
-    global_descriptor_table_t gdt;
-    kconsole << "Created GDT." << endl;
-    interrupt_descriptor_table_t::instance().install();
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x0, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x2, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x3, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x4, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x5, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x6, &iop_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x7, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x8, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x9, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xa, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xb, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xc, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xd, &gpf_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xe, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0xf, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x10, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x11, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x12, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x13, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x14, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x15, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x16, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x17, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x18, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x19, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1a, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1b, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1c, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1d, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1e, &all_exceptions_handler);
-    interrupt_descriptor_table_t::instance().set_isr_handler(0x1f, &all_exceptions_handler);
-    kconsole << "Created IDT." << endl;
+    nucleus_init();
 
     // Grab the bootinfo page and discover where is our bootimage.
     bootinfo_t* bi = new(bootinfo_t::ADDRESS) bootinfo_t;
