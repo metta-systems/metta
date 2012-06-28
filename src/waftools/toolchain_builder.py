0c17480b759349b56d72bcdb3bee108862237f99
Berkus <berkus@exquance.com>
2012-06-28 04:21:37 +0300

    Replace waf toolchain builder with a simple instruction.

--- src/waftools/toolchain_builder.py
+++ src/waftools/toolchain_builder.py
@@ -6,16 +6,8 @@ from waflib.Configure import conf
 def options(opt):
     pass
 
-# Checkout, build and install local version of clang toolchain with libc++.
-# Installation directory is srcdir+'_toolchain_' by default.
-# Will install only once, remove toolchain dir to force reinstall.
 @conf
 def ensure_local_toolchain(bld):
-    # toolchain_dir = bld.make_node('_toolchain_')
-    # print "*** Checking local toolchain presence in " + toolchain_dir + "."
-    print "*** Building local clang toolchain."
-    print "*** Building local libc++."
-
-
-# Utils.g_module.__dict__['ensure_local_toolchain'] = ensure_local_toolchain
-
+    print "******************************************************"
+    print "*** To build local toolchain run ./build_toolchain ***"
+    print "******************************************************"
