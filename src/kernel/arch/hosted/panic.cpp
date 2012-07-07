83d4784f6f98273e489f39bd432a233300c45729
Berkus <berkus@exquance.com>
2012-07-07 23:09:13 +0300

    Print backtrace when panicing.

--- src/kernel/arch/hosted/panic.cpp
+++ src/kernel/arch/hosted/panic.cpp
@@ -7,19 +7,20 @@
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include "panic.h"
+#include "debugger.h"
 #include "default_console.h"
 
 void panic(const char* message, const char* file, uint32_t line)
 {
     kconsole << "PANIC! " << message << " at " << file << ":" << (int)line << endl;
-
+    debugger_t::print_backtrace(0, 0, 20);
     halt();
 }
 
 void panic_assert(const char* desc, const char* file, uint32_t line)
 {
     kconsole << "ASSERTION FAILED! " << desc << " at " << file << ":" << (int)line << endl;
-
+    debugger_t::print_backtrace(0, 0, 20);
     halt();
 }
 
