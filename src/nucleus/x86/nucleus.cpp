f9f7ca4bcf0f33a2c6fce650e6c0e4f9eebfb55d
Berkus <berkus@exquance.com>
2012-07-16 23:07:00 +0300

    Fix ccd errors.

--- src/nucleus/x86/nucleus.cpp
+++ src/nucleus/x86/nucleus.cpp
@@ -6,6 +6,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include "nucleus.h"
 #include "cpu.h"
 /*
 static void write_pdbr_impl(address_t phys, address_t)
