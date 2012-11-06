c9f9dc323402c2a8a9b05f40b660b3d08c2e9d7b
Berkus <berkus@gmail.com>
2012-11-06 21:57:42 +0200

    WIP on exports_table.

--- src/modules/idc_mod/exports_table.cpp
+++ src/modules/idc_mod/exports_table.cpp
@@ -13,6 +13,17 @@ DECLARE_MAP(offer_table, idc_offer_v1::closure_t*, table_entry_t);
 struct exports_table_v1::state_t
 {
 	exports_table_v1::closure_t client_closure;
+	//binder_callback_closure;
 	heap_v1::closure_t* heap;
+	//binder;
+	//entry_factory;
+
+	//server_state;
+	//callback_connection;
+	//callback_entry;
+
+	//mutex;
+	bool registered;
 	offer_table_t offers;
+	//event_count;
 };
