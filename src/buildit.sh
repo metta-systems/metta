20e698cd5fbb865152a86d55bbd78c99ab7bc466
Berkus <berkus@gmail.com>
2013-06-26 02:39:59 +0400

    Split main cmake file into host and target parts.
    This needs two trees to build the whole thing, buildit.sh script helps.
    cross.toolchain tells cmake to not assume host system environment like
    OSXROOT etc.

---
+++ src/buildit.sh
@@ -0,0 +1,13 @@
+mkdir -p _build_host_ _build_target_
+
+# First build the host executables using native compiler and environment.
+cd _build_host_
+cmake -G Ninja -DCONFIG_PLATFORM=pc99 ..
+ninja
+
+# Then build the target system using cross toolchain we built.
+cd ../_build_target_
+cmake -G Ninja -DCONFIG_PLATFORM=pc99 -DCMAKE_TOOLCHAIN_FILE=../cmake/cross.toolchain -DIMPORT_EXECUTABLES=../_build_host_/ImportExecutables.cmake ..
+ninja
+
+cd ..
