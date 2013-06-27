b38c63f7b0deb720113cd2a493189e0b24051696
Berkus <berkus@gmail.com>
2013-06-28 00:18:28 +0400

    Don't run cmake if not necessary.

--- src/buildit.sh
+++ src/buildit.sh
@@ -2,12 +2,16 @@ mkdir -p _build_host_ _build_target_
 
 # First build the host executables using native compiler and environment.
 cd _build_host_
-cmake -G Ninja -DCONFIG_PLATFORM=pc99 ..
+if [ ! -f CMakeCache.txt ]; then
+	cmake -G Ninja -DCONFIG_PLATFORM=pc99 ..
+fi
 ninja
 
 # Then build the target system using cross toolchain we built.
 cd ../_build_target_
-cmake -G Ninja -DCONFIG_PLATFORM=pc99 -DCMAKE_TOOLCHAIN_FILE=../cmake/cross.toolchain -DIMPORT_EXECUTABLES=../_build_host_/ImportExecutables.cmake ..
+if [ ! -f CMakeCache.txt ]; then
+	cmake -G Ninja -DCONFIG_PLATFORM=pc99 -DCMAKE_TOOLCHAIN_FILE=../cmake/cross.toolchain -DIMPORT_EXECUTABLES=../_build_host_/ImportExecutables.cmake ..
+fi
 ninja       # FIXME: some deps are still missing from metta target
 ninja metta
 
