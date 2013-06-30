a2819f72490c2ef0ae024a0895ae7f33d3cfa311
Berkus <berkus@gmail.com>
2013-06-30 14:06:48 +0300

    Nucleus is actually a part of the init.img and need not be copied.

--- src/tools/mkbootimg/images.sh
+++ src/tools/mkbootimg/images.sh
@@ -9,7 +9,6 @@ mkdir -p iso/boot/grub
 cp `dirname $0`/stage2_eltorito           iso/boot/grub
 cp $BUILDDIR/tools/mkbootimg/menu.lst.cd  iso/boot/grub/menu.lst
 cp $BUILDDIR/launcher/launcher.comp       iso/launcher
-cp $BUILDDIR/nucleus/nucleus.comp         iso/nucleus
 cp $BUILDDIR/init.img                     iso/sysimage
 mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -o $CDISO iso
 rm -rf iso
