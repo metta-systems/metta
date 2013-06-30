5b6ea23970c1911f15f1e14af358704c84711ed9
Berkus <berkus@gmail.com>
2013-06-30 13:48:58 +0300

    Add progress messages.

--- src/buildit.sh
+++ src/buildit.sh
@@ -3,6 +3,9 @@
 mkdir -p _build_host_ _build_target_
 
 # First build the host executables using native compiler and environment.
+echo
+echo "*** Making tools"
+echo
 cd _build_host_
 if [ ! -f CMakeCache.txt ]; then
 	cmake -G Ninja -DCONFIG_PLATFORM=pc99 ..
@@ -10,6 +13,9 @@ fi
 ninja
 
 # Then build the target system using cross toolchain we built.
+echo
+echo "*** Making system"
+echo
 cd ../_build_target_
 if [ ! -f CMakeCache.txt ]; then
 	cmake -G Ninja -DCONFIG_PLATFORM=pc99 -DCMAKE_TOOLCHAIN_FILE=../cmake/cross.toolchain -DIMPORT_EXECUTABLES=../_build_host_/ImportExecutables.cmake ..
