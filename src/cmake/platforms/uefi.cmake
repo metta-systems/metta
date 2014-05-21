447c1020f4d9346042a108178c26a2503000c09a
Berkus <berkus@gmail.com>
2014-05-22 01:46:29 +0300

    Add UEFI platform.

--- src/cmake/platforms/uefi.cmake
+++ src/cmake/platforms/uefi.cmake
@@ -1,6 +1,8 @@
 message("## Configuring for UEFI platform")
 
-set(ARCH x86_64 CACHE FORCE "arch set by platform uefi.cmake")
-
 # uefi - core2duo or later, 64 bit, using uefi bootloader, gpt partition tables and all that
 
+set(TARGET "x86_64-pc-elf" CACHE FORCE "target set by platform uefi.cmake")
+set(BOARD "" CACHE FORCE "board set by platform uefi.cmake")
+set(ARCH "x86_64" CACHE FORCE "arch set by platform uefi.cmake")
+set(PLATFORM "uefi" CACHE FORCE "platform set by platform uefi.cmake")
