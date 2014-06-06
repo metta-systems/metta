0095089dede238b95d7e81188ce92564479eab19
Berkus <berkus@gmail.com>
2014-06-06 03:57:16 +0300

    Change old address.

--- src/tests/test_suite_main.cpp
+++ src/tests/test_suite_main.cpp
@@ -1,14 +1,13 @@
 //
+// @brief Main entry point.
+//
 // Part of Metta OS. Check http://atta-metta.net for latest version.
 //
-// Copyright 2007 - 2012, Stanislav Karchebnyy <berkus@exquance.com>
+// Copyright 2007 - 2012, Stanislav Karchebnyy <berkus@atta-metta.net>
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-/**
- * @brief Main entry point.
- */
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE class library tests
--- src/tools/component_source_check.rb
+++ src/tools/component_source_check.rb
@@ -5,7 +5,7 @@
 #
 # Part of Metta OS. Check http://atta-metta.net for latest version.
 #
-# Copyright 2007 - 2012, Stanislav Karchebnyy <berkus@exquance.com>
+# Copyright 2007 - 2012, Stanislav Karchebnyy <berkus@atta-metta.net>
 #
 # Distributed under the Boost Software License, Version 1.0.
 # (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
