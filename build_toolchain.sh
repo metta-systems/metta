0d5f274087fdd219ef5555bd842d01267f17f12e
Berkus Decker <berkus@madfire.net>
2015-04-22 08:31:17 +0300

    Refactor toolchain build script.

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -18,12 +18,12 @@ echo
 
 export LLVM_TARGETS="X86;ARM;AArch64;Mips"
 
-export LLVM_REVISION=29682f42
-export CLANG_REVISION=87b3c14288
-export COMPILER_RT_REVISION=b9bc8ad
+export LLVM_REVISION=master
+export CLANG_REVISION=master
+export COMPILER_RT_REVISION=master
 export LIBCXXABI_REVISION=master
-export LIBCXX_REVISION=52a708f
-export LLD_REVISION=a124f52
+export LIBCXX_REVISION=master
+export LLD_REVISION=master
 if [ -z $LIBCXX_TRIPLE ]; then
     export LIBCXX_TRIPLE=-apple-
 fi
@@ -34,151 +34,129 @@ which git || (echo "Install git: brew install git"; exit)
 which cmake || (echo "Install cmake: brew install cmake"; exit)
 which ninja || (echo "Install ninja: brew install ninja"; exit)
 
-mkdir -p toolchain/{build/llvm,sources}
+mkdir -p toolchain/{build/{llvm,libcxxabi,libcxx},sources}
 cd toolchain/
 
 export TOOLCHAIN_DIR=`pwd`
 
-echo "===================================================================="
-echo "Checking out llvm [$LLVM_REVISION] / compiler-rt [$COMPILER_RT_REVISION]..."
-echo "===================================================================="
+# *** FUNCTIONS ***
 
-if [ ! -d sources/llvm ]; then
-    git clone -n http://llvm.org/git/llvm.git sources/llvm
-    (cd sources/llvm; git checkout $LLVM_REVISION)
-else
-    (cd sources/llvm; git fetch; git checkout $LLVM_REVISION)
-fi
+# $1 name
+# $2 url
+# $3 revision
+# $4 path under sources/
+function checkout {
+    echo "===================================================================="
+    echo "Getting $1 [$3]..."
 
-if [ ! -d sources/llvm/projects/compiler-rt ]; then
-    git clone -n http://llvm.org/git/compiler-rt.git sources/llvm/projects/compiler-rt
-    (cd sources/llvm/projects/compiler-rt; git checkout $COMPILER_RT_REVISION)
-else
-    (cd sources/llvm/projects/compiler-rt; git fetch; git checkout $COMPILER_RT_REVISION)
-fi
+    if [ ! -d sources/$4 ]; then
+        git clone -n $2 sources/$4
+        (cd sources/$4; git reset --hard $3)
+    else
+        (cd sources/$4; git fetch; git reset --hard $3)
+    fi
+}
 
-echo "===================================================================="
-echo "Checking out clang [$CLANG_REVISION]..."
-echo "===================================================================="
+# $1 path/name
+# $2 cmdline
+function configure {
+    echo "===================================================================="
+    echo "Configuring $1..."
 
-if [ ! -d sources/llvm/tools/clang ]; then
-    git clone -n http://llvm.org/git/clang.git sources/llvm/tools/clang
-    (cd sources/llvm/tools/clang; git checkout $CLANG_REVISION)
-else
-    (cd sources/llvm/tools/clang; git fetch; git checkout $CLANG_REVISION)
-fi
+    if [ ! -f build/$1/.config.succeeded ]; then
+        cd build/$1 && \
+        $2 ../../sources/$1 && \
+        touch .config.succeeded && \
+        cd ../.. || exit 1
+    else
+        echo "build/$1/.config.succeeded exists, NOT reconfiguring $1!"
+    fi
+}
 
-echo "===================================================================="
-echo "Checking out lld [$LLD_REVISION]..."
-echo "===================================================================="
+# $1 path/name
+# $2 optional cmdline, default to ninja
+function build {
+    echo "===================================================================="
+    echo "Building $1..."
 
-if [ ! -d sources/llvm/tools/lld ]; then
-    git clone -n http://llvm.org/git/lld.git sources/llvm/tools/lld
-    (cd sources/llvm/tools/lld; git checkout $LLD_REVISION)
-else
-    (cd sources/llvm/tools/lld; git fetch; git checkout $LLD_REVISION)
-fi
+    cmd="ninja"
+    if [ -n "$2" ]
+    then
+        cmd="$2"
+    fi
 
-echo "===================================================================="
-echo "Checking out recent libc++abi [$LIBCXXABI_REVISION]..."
-echo "===================================================================="
+    if [ ! -f build/$1/.build.succeeded ]; then
+        cd build/$1 && \
+        $cmd && \
+        touch .build.succeeded && \
+        cd ../.. || exit 1
+    else
+        echo "build/$1/.build.succeeded exists, NOT rebuilding $1!"
+    fi
+}
 
-if [ ! -d libcxxabi ]; then
-    git clone -n http://llvm.org/git/libcxxabi.git libcxxabi
-    (cd libcxxabi; git checkout $LIBCXXABI_REVISION)
-else
-    (cd libcxxabi; git fetch; git checkout $LIBCXXABI_REVISION)
-fi
+# $1 path/name
+# $2 optional cmdline, default to ninja install
+# $3 optional title (use $1 if empty)
+function install {
+    echo "===================================================================="
+    if [ -z "$3" ]
+    then
+        echo "Installing $1..."
+    else
+        echo "Installing $3..."
+    fi
 
-echo "===================================================================="
-echo "Checking out recent libcxx [$LIBCXX_REVISION]..."
-echo "===================================================================="
+    cmd="ninja install"
+    if [ -n "$2" ]
+    then
+        cmd="$2"
+    fi
 
-if [ ! -d libcxx ]; then
-    git clone -n http://llvm.org/git/libcxx.git libcxx
-    (cd libcxx; git checkout $LIBCXX_REVISION)
-else
-    (cd libcxx; git fetch; git checkout $LIBCXX_REVISION)
-fi
+    if [ ! -f build/$1/.install.succeeded ]; then
+        cd build/$1 && \
+        $cmd && \
+        touch .install.succeeded && \
+        cd ../.. || exit 1
+    else
+        echo "build/$1/.install.succeeded exists, NOT reinstalling $1!"
+    fi
+}
+
+# END OF FUNCTIONS
+
+#
+# Main action
+#
+checkout "llvm" "http://llvm.org/git/llvm.git" "$LLVM_REVISION" "llvm"
+checkout "compiler-rt" "http://llvm.org/git/compiler-rt.git" "$COMPILER_RT_REVISION" "llvm/projects/compiler-rt"
+checkout "clang" "http://llvm.org/git/clang.git" "$CLANG_REVISION" "llvm/tools/clang"
+checkout "lld" "http://llvm.org/git/lld.git" "$LLD_REVISION" "llvm/tools/lld"
+checkout "libc++abi" "http://llvm.org/git/libcxxabi.git" "$LIBCXXABI_REVISION" "libcxxabi"
+checkout "libc++" "http://llvm.org/git/libcxx.git" "$LIBCXX_REVISION" "libcxx"
 
 if [ -n "$FETCH_ONLY" ] && [ "$FETCH_ONLY" -eq "1" ]; then
     echo "Fetch complete."
     exit 0
 fi
 
-echo "===================================================================="
-echo "Configuring llvm..."
-echo "===================================================================="
+configure "llvm" "cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/clang \
+    -DLLVM_ENABLE_CXX1Y=ON -DLLVM_ENABLE_LIBCXX=ON \
+    -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS"
+build "llvm"
+install "llvm" "ninja install" "llvm, clang & lld"
 
-if [ ! -f build/llvm/.config.succeeded ]; then
-    cd build/llvm && \
-    cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/clang \
-    -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++ -DLLVM_ENABLE_DUMP" \
-    -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS ../../sources/llvm && \
-    touch .config.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "build/llvm/.config.succeeded exists, NOT reconfiguring llvm!"
-fi
+configure "libcxxabi" "cmake -G Ninja -DLLVM_CONFIG=$TOOLCHAIN_DIR/clang/bin/llvm-config \
+    -DLIBCXXABI_LIBCXX_INCLUDES=$TOOLCHAIN_DIR/sources/libcxx/include \
+    -DCMAKE_BUILD_TYPE=Release -DLLVM_ABI_BREAKING_CHECKS=WITH_ASSERTS \
+    -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/libc++abi"
+build "libcxxabi"
+install "libcxxabi"
 
-echo "===================================================================="
-echo "Building llvm..."
-echo "===================================================================="
-
-if [ ! -f build/llvm/.build.succeeded ]; then
-    cd build/llvm && \
-    ninja && \
-    touch .build.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "build/llvm/.build.succeeded exists, NOT rebuilding llvm!"
-fi
-
-echo "===================================================================="
-echo "Installing llvm, clang & lld..."
-echo "===================================================================="
-
-if [ ! -f build/llvm/.install.succeeded ]; then
-    cd build/llvm && \
-    ninja install && \
-    touch .install.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "build/llvm/.install.succeeded exists, NOT reinstalling llvm!"
-fi
-
-echo "===================================================================="
-echo "Configuring and building libc++abi..."
-echo "===================================================================="
-
- # /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/5.1/include
- # /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include
- # /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frame
-
-# if [ ! -f libcxxabi/.build.succeeded ]; then
-#     cd libcxxabi/lib && \
-#     TRIPLE=$LIBCXX_TRIPLE CC=$TOOLCHAIN_DIR/clang/bin/clang CXX=$TOOLCHAIN_DIR/clang/bin/clang++ \
-#     ./buildit && \
-#     touch ../.build.succeeded && \
-#     cd ../.. || exit 1
-# else
-#     echo "libcxxabi/.build.succeeded exists, NOT rebuilding libc++abi!"
-# fi
-
-echo "===================================================================="
-echo "Configuring and building libcxx..."
-echo "===================================================================="
-
-if [ ! -f libcxx/.build.succeeded ]; then
-    cd libcxx/lib && \
-    TRIPLE=$LIBCXX_TRIPLE CC=$TOOLCHAIN_DIR/clang/bin/clang \
-    CXX="$TOOLCHAIN_DIR/clang/bin/clang++ -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include" \
-    ./buildit && \
-    touch ../.build.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "libcxx/.build.succeeded exists, NOT rebuilding libcxx!"
-fi
+configure "libcxx" "cmake -G Ninja -DLIBCXX_ENABLE_CXX1Y=ON CC=$TOOLCHAIN_DIR/clang/bin/clang \
+    CXX=$TOOLCHAIN_DIR/clang/bin/clang++ -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/libc++"
+build "libcxx"
+install "libcxx"
 
 echo "===================================================================="
 echo "===================================================================="
@@ -188,72 +166,47 @@ echo "===================================================================="
 
 # TODO: copy libc++.so.1 to clang bin directory for Linux building or adjust LD_LIBRARY_PATH...
 
-echo "===================================================================="
-echo "Configuring llvm..."
-echo "===================================================================="
-
 # We rebuild using just built fresh clang for the sole reason of being able
 # to use recent libcxx (which we link against in tools), so LLVM libs have
 # to be built against this same libcxx too.
 
-# Check if polly and lld can be built with this llvm version without errors
-# and enable:
-# --enable-polly
+export EXTRA_OPTIONS="-I$TOOLCHAIN_DIR/libcxx/include"
+export EXTRA_LD_OPTIONS="-L$TOOLCHAIN_DIR/libcxx/lib -lc++"
+
+configure "llvm2", ""
 
 # Force use of local libcxx for new clang build.
 # This doesn't enable the options, merely records them, the real activation
 # happens below in make command invocation.
-    # cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/clang -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS ../../sources/llvm && \
-
-export EXTRA_OPTIONS="-I$TOOLCHAIN_DIR/libcxx/include"
-export EXTRA_LD_OPTIONS="-L$TOOLCHAIN_DIR/libcxx/lib -lc++"
 
 if [ ! -f build/llvm2/.config2.succeeded ]; then
     cd build/llvm2 && \
     CC=$TOOLCHAIN_DIR/clang/bin/clang CXX=$TOOLCHAIN_DIR/clang/bin/clang++ \
+
     ../../sources/llvm/configure --prefix=$TOOLCHAIN_DIR/clang/ --enable-jit --enable-optimized \
     --enable-libcpp --disable-docs \
     --with-binutils-include=$TOOLCHAIN_DIR/sources/binutils-${BINUTILS_VER}/include/ --enable-pic \
     --enable-targets=$LLVM_TARGETS  && \
+
     touch .config2.succeeded && \
     cd ../.. || exit 1
 else
     echo "build/llvm2/.config2.succeeded exists, NOT reconfiguring llvm!"
 fi
 
-echo "===================================================================="
-echo "Building llvm..."
-echo "===================================================================="
+build "llvm2"
+"make -j$MAKE_THREADS EXTRA_OPTIONS="$EXTRA_OPTIONS" EXTRA_LD_OPTIONS="$EXTRA_LD_OPTIONS" && \
+    make check"
 
-if [ ! -f build/llvm2/.build2.succeeded ]; then
-    cd build/llvm2 && \
-    make -j$MAKE_THREADS EXTRA_OPTIONS="$EXTRA_OPTIONS" EXTRA_LD_OPTIONS="$EXTRA_LD_OPTIONS" && \
-    make check && \
-    touch .build2.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "build/llvm2/.build2.succeeded exists, NOT rebuilding llvm!"
-fi
-
-echo "===================================================================="
-echo "Installing llvm & clang..."
-echo "===================================================================="
-
-if [ ! -f build/llvm2/.install2.succeeded ]; then
-    cd build/llvm2 && \
+install "llvm2" "" "llvm & clang"
     make install EXTRA_OPTIONS="$EXTRA_OPTIONS" EXTRA_LD_OPTIONS="$EXTRA_LD_OPTIONS" && \
-    touch .install2.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "build/llvm2/.install2.succeeded exists, NOT reinstalling llvm!"
-fi
 
 echo "===================================================================="
 echo "To clean up:"
 echo "cd toolchain"
 echo "rm -rf build sources"
 echo
-echo "Toolchain binaries will remain in clang/ and libcxx/"
+echo "Toolchain binaries will remain in clang/, libcxxabi/ and libcxx/"
 echo "where Metta configure will find them."
 echo "===================================================================="
 echo
@@ -263,4 +216,3 @@ echo "All done, enjoy!"
 echo "===================================================================="
 echo "===================================================================="
 cd ..
-
