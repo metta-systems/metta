9685cb490f2f1d92f53e39f72401df9d7d0eeacb
Berkus Decker <berkus@madfire.net>
2017-04-12 15:34:58 +0300

    Modernise toolchain build script for latest LLVM, add Polly

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -17,11 +17,13 @@ echo
 
 export LLVM_TARGETS="X86;ARM;AArch64;Mips"
 
-export LLVM_REVISION=52244da
-export CLANG_REVISION=82a2911
-export COMPILER_RT_REVISION=8e22b68
-export LIBCXX_REVISION=71b5215
-export LLD_REVISION=0d900a1
+export LLVM_REVISION=origin/master
+export CLANG_REVISION=origin/master
+export LLD_REVISION=origin/master
+export POLLY_REVISION=origin/master
+export COMPILER_RT_REVISION=origin/master
+export LIBCXX_REVISION=origin/master
+export LIBCXXABI_REVISION=origin/master
 if [ -z $LIBCXX_TRIPLE ]; then
     export LIBCXX_TRIPLE=-apple-
 fi
@@ -32,24 +34,26 @@ which git || (echo "Install git: brew install git"; exit)
 which cmake || (echo "Install cmake: brew install cmake"; exit)
 which ninja || (echo "Install ninja: brew install ninja"; exit)
 
-mkdir -p toolchain/{build/{llvm},sources}
+mkdir -p toolchain/{build/llvm,sources}
 cd toolchain/
 
 export TOOLCHAIN_DIR=`pwd`
+
+REPOBASE=https://github.com/llvm-mirror
 
 echo "===================================================================="
 echo "Checking out llvm [$LLVM_REVISION] / compiler-rt [$COMPILER_RT_REVISION]..."
 echo "===================================================================="
 
 if [ ! -d sources/llvm ]; then
-    git clone http://llvm.org/git/llvm.git sources/llvm
+    git clone $REPOBASE/llvm.git sources/llvm
     (cd sources/llvm; git checkout $LLVM_REVISION)
 else
     (cd sources/llvm; git fetch; git checkout $LLVM_REVISION)
 fi
 
 if [ ! -d sources/llvm/projects/compiler-rt ]; then
-    git clone http://llvm.org/git/compiler-rt.git sources/llvm/projects/compiler-rt
+    git clone $REPOBASE/compiler-rt.git sources/llvm/projects/compiler-rt
     (cd sources/llvm/projects/compiler-rt; git checkout $COMPILER_RT_REVISION)
 else
     (cd sources/llvm/projects/compiler-rt; git fetch; git checkout $COMPILER_RT_REVISION)
@@ -60,7 +64,7 @@ echo "Checking out clang [$CLANG_REVISION]..."
 echo "===================================================================="
 
 if [ ! -d sources/llvm/tools/clang ]; then
-    git clone http://llvm.org/git/clang.git sources/llvm/tools/clang
+    git clone $REPOBASE/clang.git sources/llvm/tools/clang
     (cd sources/llvm/tools/clang; git checkout $CLANG_REVISION)
 else
     (cd sources/llvm/tools/clang; git fetch; git checkout $CLANG_REVISION)
@@ -71,21 +75,39 @@ echo "Checking out lld [$LLD_REVISION]..."
 echo "===================================================================="
 
 if [ ! -d sources/llvm/tools/lld ]; then
-    git clone http://llvm.org/git/lld.git sources/llvm/tools/lld
+    git clone $REPOBASE/lld.git sources/llvm/tools/lld
     (cd sources/llvm/tools/lld; git checkout $LLD_REVISION)
 else
     (cd sources/llvm/tools/lld; git fetch; git checkout $LLD_REVISION)
 fi
 
 echo "===================================================================="
-echo "Checking out recent libcxx r$LIBCXX_REVISION..."
+echo "Checking out polly [$POLLY_REVISION]..."
 echo "===================================================================="
 
-if [ ! -d libcxx ]; then
-    git clone http://llvm.org/git/libcxx.git libcxx
-    (cd libcxx; git checkout $LIBCXX_REVISION)
+if [ ! -d sources/llvm/tools/polly ]; then
+    git clone $REPOBASE/polly.git sources/llvm/tools/polly
+    (cd sources/llvm/tools/polly; git checkout $POLLY_REVISION)
 else
-    (cd libcxx; git fetch; git checkout $LIBCXX_REVISION)
+    (cd sources/llvm/tools/polly; git fetch; git checkout $POLLY_REVISION)
+fi
+
+echo "===================================================================="
+echo "Checking out recent libcxx [$LIBCXX_REVISION] libcxxabi [$LIBCXXABI_REVISION]..."
+echo "===================================================================="
+
+if [ ! -d sources/llvm/projects/libcxx ]; then
+    git clone $REPOBASE/libcxx.git sources/llvm/projects/libcxx
+    (cd sources/llvm/projects/libcxx; git checkout $LIBCXX_REVISION)
+else
+    (cd sources/llvm/projects/libcxx; git fetch; git checkout $LIBCXX_REVISION)
+fi
+
+if [ ! -d sources/llvm/projects/libcxxabi ]; then
+    git clone $REPOBASE/libcxxabi.git sources/llvm/projects/libcxxabi
+    (cd sources/llvm/projects/libcxxabi; git checkout $LIBCXXABI_REVISION)
+else
+    (cd sources/llvm/projects/libcxxabi; git fetch; git checkout $LIBCXXABI_REVISION)
 fi
 
 echo "===================================================================="
@@ -94,7 +116,7 @@ echo "===================================================================="
 
 if [ ! -f build/llvm/.config.succeeded ]; then
     cd build/llvm && \
-    cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/clang -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS ../../sources/llvm && \
+    cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/clang -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" -DLLVM_TARGETS_TO_BUILD=$LLVM_TARGETS -DLLVM_CREATE_XCODE_TOOLCHAIN=ON ../../sources/llvm && \
     touch .config.succeeded && \
     cd ../.. || exit 1
 else
@@ -115,7 +137,7 @@ else
 fi
 
 echo "===================================================================="
-echo "Installing llvm, clang & lld..."
+echo "Installing llvm, libcxx, clang & lld..."
 echo "===================================================================="
 
 if [ ! -f build/llvm/.install.succeeded ]; then
@@ -127,19 +149,7 @@ else
     echo "build/llvm/.install.succeeded exists, NOT reinstalling llvm!"
 fi
 
-echo "===================================================================="
-echo "Configuring and building libcxx..."
-echo "===================================================================="
-
-if [ ! -f libcxx/.build.succeeded ]; then
-    cd libcxx/lib && \
-    TRIPLE=$LIBCXX_TRIPLE CC=$TOOLCHAIN_DIR/clang/bin/clang CXX=$TOOLCHAIN_DIR/clang/bin/clang++ \
-    ./buildit && \
-    touch ../.build.succeeded && \
-    cd ../.. || exit 1
-else
-    echo "libcxx/.build.succeeded exists, NOT rebuilding libcxx!"
-fi
+exit 0
 
 echo "===================================================================="
 echo "===================================================================="
